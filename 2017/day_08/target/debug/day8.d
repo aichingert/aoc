714055eae45c611da65acdc805957961749946a1
TobiasAichinger <aichinger.tobias13@gmail.com>
2022-05-29 20:31:05 +0200

    removed day8

--- 2017/day_08/target/debug/day8.d
+++
@@ -1,1 +0,0 @@
-C:\projects\Advent-of-Code-Rust\2017\day8\target\debug\day8.exe: C:\projects\Advent-of-Code-Rust\2017\day8\src\main.rs
--- 2017/day_08/target/debug/deps/day8-f4a139b040ac3d89.d
+++
@@ -1,5 +0,0 @@
-c:\projects\Advent-of-Code-Rust\2017\day8\target\debug\deps\day8-f4a139b040ac3d89.rmeta: src\main.rs
-
-c:\projects\Advent-of-Code-Rust\2017\day8\target\debug\deps\day8-f4a139b040ac3d89.d: src\main.rs
-
-src\main.rs:
