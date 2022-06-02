e2e26e8467fc4b842a75fce1a58f2c1c3ce4a683
Tobias <aichinger.tobias13@gmail.com>
2022-06-02 10:45:50 +0200

    day10

--- 2017/day_10/src/main.rs
+++ 2017/day_10/src/main.rs
@@ -55,15 +55,41 @@ fn solve_part_two(content: String) {
     let mut changing: usize = 0;
 
     let mut numbers: Vec<_> = content.chars().map( | str | {
-        str as i32
+        str as usize
     }).collect();
-    let prefixes: Vec<i32> = vec![17, 31, 73, 47, 23];
+    let prefixes: Vec<usize> = vec![17, 31, 73, 47, 23];
 
     for prefix in prefixes {
         numbers.push(prefix)
     }
 
-    println!("{:?}", numbers)
+    for _ in 0..64 {
+        for number in numbers.clone() {
+            min = changing;
+    
+            changing += skip_size + number;
+            skip_size += 1;
+    
+            changing %= SIZE;
+    
+            range = number;
+    
+            let cut = reverse(&mut min, &mut range, &mut elements);
+    
+            let mut min_idx = min;
+    
+            for i in 0..cut.len() {
+                if min_idx >= elements.len() {
+                    min_idx = 0;
+                }
+    
+                elements[min_idx] = cut[i];
+                min_idx += 1;
+            }
+        }
+    }
+
+    println!("Solution part 2: {}", generate_hash(dense_hash(&mut elements)));
 }
 
 fn reverse(min: &mut usize, number_range: &mut usize, elements: &mut [i32; SIZE]) -> Vec<i32> {
@@ -90,28 +116,30 @@ fn reverse(min: &mut usize, number_range: &mut usize, elements: &mut [i32; SIZE]
     cut
 }
 
-/*      min = skip_size;
-        skip_size = max;
-        max = skip_size + number;
+fn dense_hash(elements: &mut [i32; SIZE]) -> Vec<i32> {
+    let mut hash_numbers: Vec<i32> = Vec::new();
+    let mut hexed_number: i32 = 0;
 
-        max %= SIZE;
+    for i in 0..elements.len()/16 {
+        let idx = i * 16;
 
+        hexed_number = elements[idx] ^ elements[idx + 1] ^ elements[idx + 2] ^ elements[idx + 3] ^ elements[idx + 4] ^ elements[idx + 5] ^ elements[idx + 6] ^ elements[idx + 7] ^ elements[idx + 8] ^ elements[idx + 9] ^ elements[idx + 10] ^ elements[idx + 11] ^ elements[idx + 12] ^ elements[idx + 13] ^ elements[idx + 14] ^ elements[idx + 15];
+        hash_numbers.push(hexed_number);
+    }
 
-        println!("{} ", skip_size);
-        
-        let cut = reverse(&mut min, &mut max, &mut elements);
+    hash_numbers
+}
 
-        let mut min_idx = min;
+fn generate_hash(dense_hash: Vec<i32>) -> String {
+    let mut hash: String = String::new();
 
-        for i in 0..cut.len() {
-            if min_idx >= elements.len() {
-                min_idx = 0;
-            }
+    for i in 0..dense_hash.len() {
+       hash.push_str(&to_hex(dense_hash[i]));
+    }
 
-            elements[min_idx] = cut[i];
-            min_idx += 1;
-        }
+    hash
+}
 
-        skip_size += 1;
-        skip_size %= SIZE; 
-*/
+fn to_hex (value: i32) -> String {
+    format!("{:02X}", value)
+}
