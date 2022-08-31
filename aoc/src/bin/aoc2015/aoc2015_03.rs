aa88b94bdfbd2f6464b0fed94bfc9d465d0cace2
TobiasAichinger <aichinger.tobias13@gmail.com>
2022-08-31 22:01:40 +0200

    day3

---
+++ aoc/src/bin/aoc2015/aoc2015_03.rs
@@ -0,0 +1,65 @@
+use aoc::read_to_chars;
+use std::collections::HashSet;
+
+use crate::Solution;
+
+pub struct Aoc2015_03 {
+    d: Vec<char>
+}
+
+impl Aoc2015_03 {
+    pub fn new() -> Self {
+        Self { d: vec![] }
+    }
+}
+
+impl crate::Solution for Aoc2015_03 {
+    fn name(&self) -> (usize, usize) {
+        (2015, 3)
+    }
+
+    fn parse(&mut self) {
+        self.d = read_to_chars("input/2015/03.txt")
+    }
+
+    fn part1(&mut self) -> Vec<String> {
+        let mut m: HashSet<(i32, i32)> = HashSet::new();
+        let mut x: i32 = 0;
+        let mut y: i32 = 0;
+
+        for i in 0..self.d.len() {
+            m.insert((x, y));
+            match self.d[i] {
+                '<' => x -= 1,
+                '>' => x += 1,
+                '^' => y += 1,
+                'v' => y -= 1,
+                _ => panic!("invalid char in input")
+            }
+        }
+        m.insert((x, y));
+
+        crate::output(m.len())
+    }
+
+    fn part2(&mut self) -> Vec<String> {
+        let mut m: HashSet<(i32, i32)> = HashSet::new();
+        let mut x = [0, 0];
+        let mut y = [0, 0];
+        let mut w: usize = 0;
+        for i in 0..self.d.len() {
+            m.insert((x[w], y[w]));
+            match self.d[i] {
+                '<' => x[w] -= 1,
+                '>' => x[w] += 1,
+                '^' => y[w] += 1,
+                'v' => y[w] -= 1,
+                _ => panic!("invalid char in input")
+            }
+            w = 1 - w;
+        }
+        m.insert((x[w], y[w]));
+
+        crate::output(m.len())
+    }
+}
--- aoc/src/bin/aoc2015/mod.rs
+++ aoc/src/bin/aoc2015/mod.rs
@@ -1,15 +1,19 @@
 mod aoc2015_01;
 mod aoc2015_02;
+mod aoc2015_03;
 
 use aoc2015_01::*;
 use aoc2015_02::*;
+use aoc2015_03::*;
 
 use crate::run_solution;
 
 pub fn run_2015() {
     let mut day_01 = Aoc2015_01::new();
     let mut day_02 = Aoc2015_02::new();
+    let mut day_03 = Aoc2015_03::new();
 
     run_solution(&mut day_01);
     run_solution(&mut day_02);
+    run_solution(&mut day_03);
 }
