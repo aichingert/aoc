28569b2630b4a1149a407b9bc7e65fce4f75dcc9
TobiasAichinger <aichinger.tobias13@gmail.com>
2022-09-02 15:06:33 +0200

    2020 day 2

--- aoc/src/bin/aoc2020/README.md
+++ aoc/src/bin/aoc2020/README.md
@@ -3,3 +3,4 @@
 | day | name | solution |
 | --- | --- | --- |
 | 1 | **[Report Repair](https://adventofcode.com/2020/day/1)** | [day01](/aoc/src/bin/aoc2020/aoc2020_01.rs) |
+| 2 | **[Password Philosophy](https://adventofcode.com/2020/day/2)** | [day02](/aoc/src/bin/aoc2020/aoc2020_02.rs) |
--- aoc/src/bin/aoc2020/mod.rs
+++ aoc/src/bin/aoc2020/mod.rs
@@ -1,11 +1,15 @@
 mod aoc2020_01;
+mod aoc2020_02;
 
 use aoc2020_01::*;
+use aoc2020_02::*;
 
 use crate::run_solution;
 
 pub fn run_2020() {
     let mut day_01 = Aoc2020_01::new();
+    let mut day_02 = Aoc2020_02::new();
 
     run_solution(&mut day_01);
+    run_solution(&mut day_02);
 }
