7899c6ae0451408d043d9f3c6e01fcc49de83d43
Tobias <aichinger.tobias13@gmail.com>
2022-09-05 22:56:05 +0200

    2019 day 4

--- aoc/src/bin/aoc2019/aoc2019_04.rs
+++ aoc/src/bin/aoc2019/aoc2019_04.rs
@@ -47,6 +47,52 @@ impl crate::Solution for Aoc2019_04 {
     }
 
     fn part2(&mut self) -> Vec<String> {
-        todo!()
+        let b: Vec<usize> = self.d.iter().map( | x | x.parse::<usize>().expect("invalid input")).collect();
+        let mut n: i32 = 0;
+
+        for i in b[0]..b[1] {
+            let c: Vec<char> = i.to_string().chars().collect();
+            let mut d: bool = false;
+
+            for j in 0..c.len() -1 {
+                if c[j] <= c[j + 1] {
+                    if c[j] == c[j + 1] {
+                        d = true;
+                    }
+                } else {
+                    d = false;
+                    break;
+                }
+            }
+
+            let mut dd: bool = false;
+
+            if d {
+                let mut j: usize = 0;
+                while j < c.len() {
+                    let mut l: usize = 1;
+
+                    for k in j + 1..c.len() {
+                        if c[j] == c[k] {
+                            l += 1;
+                        } else {
+                            break;
+                        }
+                    }
+
+                    j += l;
+
+                    if l == 2 {
+                        dd = true;
+                        break;
+                    }
+                }
+        
+                if dd {
+                    n += 1;
+                }
+            }
+        }
+        crate::output(n)
     }
 }
