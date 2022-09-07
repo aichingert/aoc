fb646fe18a2fc3284ed9edab754ea724b98d75b3
Tobias <aichinger.tobias13@gmail.com>
2022-09-07 10:35:21 +0200

    2021 day 6 part 1

--- aoc/src/bin/aoc2021/aoc2021_06.rs
+++ aoc/src/bin/aoc2021/aoc2021_06.rs
@@ -1,7 +1,7 @@
 use aoc::numbers;
 
 pub struct Aoc2021_06 {
-    d: Vec<Vec<i32>>
+    d: Vec<Vec<usize>>
 }
 
 impl Aoc2021_06 {
@@ -20,7 +20,24 @@ impl crate::Solution for Aoc2021_06 {
     }
 
     fn part1(&mut self) -> Vec<String> {
-        crate::output("")
+        let mut l: [i32; 9] = [0; 9];
+
+        for i in 0..self.d[0].len() {
+            l[self.d[0][i]] += 1;
+        }
+
+        for _ in 0..80 {
+            let n = l[0];
+
+            for i in 1..l.len() {
+                l[i - 1] = l[i];
+            }
+
+            l[6] += n;
+            l[8] = n;
+        }
+
+        crate::output(l.iter().sum::<i32>())
     }
 
     fn part2(&mut self) -> Vec<String> {
