cae908eab4507d17acbea434db7c332aa15c5626
aichingert <aichinger.tobias13@gmail.com>
2022-11-27 20:43:10 +0100

    2021 day 9 part 1

--- aoc/src/bin/aoc2021/aoc2021_09.rs
+++ aoc/src/bin/aoc2021/aoc2021_09.rs
@@ -1,10 +1,10 @@
 pub struct Aoc2021_09 {
-    d: Vec<i32>
+    m: Vec<Vec<u8>>
 }
         
 impl Aoc2021_09 {
     pub fn new() -> Self {
-        Self { d: vec![] }
+        Self { m: vec![] }
     }
 }
         
@@ -14,11 +14,23 @@ impl crate::Solution for Aoc2021_09 {
     }
         
     fn parse(&mut self) {
-        self.d = aoc::read_to_numbers("input/2021/09.txt");
+        self.m = aoc::read_to_map("input/2021/09.txt");
     }
         
     fn part1(&mut self) -> Vec<String> {
-        crate::output("")
+        let mut risk: i32 = 0;
+        for i in 0..self.m.len() {
+            for j in 0..self.m[i].len() {
+                if (i + 1 == self.m.len() || self.m[i][j] < self.m[i+1][j])
+                && (j + 1 == self.m[i].len() || self.m[i][j] < self.m[i][j+1])
+                && (i as i32 - 1 == -1 || self.m[i][j] < self.m[i-1][j])
+                && (j as i32 - 1 == -1 || self.m[i][j] < self.m[i][j-1]) {
+                    risk += self.m[i][j] as i32 +1;
+                }
+           }
+        }
+
+        crate::output(risk)
     }
         
     fn part2(&mut self) -> Vec<String> {
