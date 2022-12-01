8f7c9f7d50ba8173cf039eb904f9e37c57f61d58
aichingert <aichinger.tobias13@gmail.com>
2022-12-01 19:00:14 +0100

    clean up 2022 day 1

--- aoc/src/bin/aoc2022/aoc2022_01.rs
+++ aoc/src/bin/aoc2022/aoc2022_01.rs
@@ -15,25 +15,18 @@ impl crate::Solution for Aoc2022_01 {
         
     fn parse(&mut self) {
         let input: String = std::fs::read_to_string("input/2022/01.txt").unwrap();
-        let mut s: u32 = 0;
 
-        for line in input.lines() {
-            match line.is_empty() {
-                true => {
-                    if s > self.d[0] {
-                        self.d[2] = self.d[1];
-                        self.d[1] = self.d[0];
-                        self.d[0] = s;
-                    } else if s > self.d[1] {
-                        self.d[2] = self.d[1];
-                        self.d[1] = s;
-                    } else if s > self.d[2] {
-                        self.d[2] = s;
-                    }
-
-                    s = 0;
-                }
-                false => s += line.parse::<u32>().expect("invalid input"),
+        for block in input.split("\n\n") {
+            let s: u32 = block.lines().map(|v| v.parse::<u32>().expect("invalid input")).sum::<u32>();
+            if s > self.d[0] {
+                self.d[2] = self.d[1];
+                self.d[1] = self.d[0];
+                self.d[0] = s;
+            } else if s > self.d[1] {
+                self.d[2] = self.d[1];
+                self.d[1] = s;
+            } else if s > self.d[2] {
+                self.d[2] = s;
             }
         }
     }
