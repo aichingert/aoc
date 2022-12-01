4482a4beefc4ba899e78f96df4d6d35dd1dbe1e2
Tobias <aichinger.tobias13@gmail.com>
2022-12-01 06:24:16 +0100

    2022 day 1 part 1 (stable)

--- aoc/src/bin/aoc2022/aoc2022_01.rs
+++ aoc/src/bin/aoc2022/aoc2022_01.rs
@@ -14,11 +14,23 @@ impl crate::Solution for Aoc2022_01 {
     }
         
     fn parse(&mut self) {
-        let input: Vec<Vec<i32>> = aoc::numbers("input/2022/01.txt", " ");
+        let input: String = std::fs::read_to_string("input/2022/01.txt").unwrap();
+        self.d.push(0);
+
+        for line in input.lines() {
+            if line.is_empty() {
+                self.d.push(0);
+            } else {
+                let len = self.d.len()-1;
+                self.d[len] += line.parse::<i32>().unwrap();
+            }
+        }
+
+        self.d.sort();
     }
         
     fn part1(&mut self) -> Vec<String> {
-        crate::output("")
+        crate::output(self.d[self.d.len()-1])
     }
         
     fn part2(&mut self) -> Vec<String> {
