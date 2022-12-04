f9306c24c6cb80fd35d47cea363bb16625b58c29
aichingert <aichinger.tobias13@gmail.com>
2022-12-04 15:36:41 +0100

    2020 day 17 part 2

--- aoc/src/bin/aoc2020/aoc2020_17.rs
+++ aoc/src/bin/aoc2020/aoc2020_17.rs
@@ -1,14 +1,16 @@
 use std::collections::HashMap;
 
 pub struct Aoc2020_17 {
-    cubes: HashMap<(i32, i32, i32), bool>,
+    cubes_3d: HashMap<(i32, i32, i32), bool>,
+    cubes_4d: HashMap<(i32,i32,i32,i32), bool>,
     size: i32
 }
         
 impl Aoc2020_17 {
     pub fn new() -> Self {
         Self { 
-            cubes: HashMap::new(),
+            cubes_3d: HashMap::new(),
+            cubes_4d: HashMap::new(),
             size: 0
         }
     }
@@ -27,7 +29,8 @@ impl crate::Solution for Aoc2020_17 {
         for x in 0..input.len() {
             for (y,c) in input[x].chars().enumerate() {
                 if c == '#' {
-                    self.cubes.insert((x as i32,y as i32,0), true);
+                    self.cubes_3d.insert((x as i32,y as i32,0), true);
+                    self.cubes_4d.insert((x as i32, y as i32, 0, 0), true);
                 }
             }
         }
@@ -52,14 +55,14 @@ impl crate::Solution for Aoc2020_17 {
                                         continue;
                                     }
 
-                                    if self.cubes.contains_key(&(x+xx, y+yy, z+zz)) {
+                                    if self.cubes_3d.contains_key(&(x+xx, y+yy, z+zz)) {
                                         neighbours += 1;
                                     }
                                 }
                             }
                         }
 
-                        if self.cubes.contains_key(&(x, y, z)) && neighbours >= 2 && neighbours <= 3 {
+                        if self.cubes_3d.contains_key(&(x, y, z)) && neighbours >= 2 && neighbours <= 3 {
                             new_cubes.insert((x,y,z), true);
                         } else if neighbours == 3 {
                             new_cubes.insert((x,y,z), true);
@@ -68,13 +71,54 @@ impl crate::Solution for Aoc2020_17 {
                 }
             }
 
-            self.cubes = new_cubes;
+            self.cubes_3d = new_cubes;
         }
 
-        crate::output(self.cubes.len())
+        crate::output(self.cubes_3d.len())
     }
         
     fn part2(&mut self) -> Vec<String> {
-        crate::output("")
+        let mut size: i32 = self.size;
+
+        for _cycles in 0..6 {
+            let mut new_cubes: HashMap<(i32, i32, i32, i32), bool> = HashMap::new();
+            size += 2;
+
+            for x in -size..size {
+                for y in -size..size {
+                    for z in -size..size {
+                        for w in -size..size {
+                            let mut neighbours: u8 = 0;
+
+                            for xx in -1..2 {
+                                for yy in -1..2 {
+                                    for zz in -1..2 {
+                                        for ww in -1..2 {
+                                            if yy == 0 && xx == 0 && zz == 0 && ww == 0 {
+                                                continue;
+                                            }
+        
+                                            if self.cubes_4d.contains_key(&(x+xx, y+yy, z+zz, w+ww)) {
+                                                neighbours += 1;
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+    
+                            if self.cubes_4d.contains_key(&(x, y, z, w)) && neighbours >= 2 && neighbours <= 3 {
+                                new_cubes.insert((x,y,z,w), true);
+                            } else if neighbours == 3 {
+                                new_cubes.insert((x,y,z,w), true);
+                            }
+                        }
+                    }
+                }
+            }
+
+            self.cubes_4d = new_cubes;
+        }
+
+        crate::output(self.cubes_4d.len())
     }
 }
