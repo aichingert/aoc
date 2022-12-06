12c4d21b7c51f30895d10e05d717167c21e61f3f
Tobias <aichinger.tobias13@gmail.com>
2022-12-06 07:12:40 +0100

    2022 day 6 (stable): part 1

---
+++ aoc/src/bin/aoc2022/aoc2022_06.rs
@@ -0,0 +1,37 @@
+pub struct Aoc2022_06 {
+    d: Vec<char>
+}
+        
+impl Aoc2022_06 {
+    pub fn new() -> Self {
+        Self { d: vec![] }
+    }
+}
+        
+impl crate::Solution for Aoc2022_06 {
+    fn name(&self) -> (usize, usize) {
+        (2022, 06)
+    }
+        
+    fn parse(&mut self) {
+        self.d = aoc::read_to_chars("input/2022/06.txt");
+    }
+        
+    fn part1(&mut self) -> Vec<String> {
+        for i in 0..self.d.len() - 3 {
+            let mut c: Vec<char> = Vec::new();
+            let mut dup: bool = false;
+            self.d[i..=i+3].to_vec().iter().for_each(|ch| if c.contains(ch) { dup = true; } else { c.push(*ch); });
+
+            if !dup {
+                return crate::output(i+4);
+            }
+        }
+        
+        panic!("invalid input!");
+    }
+        
+    fn part2(&mut self) -> Vec<String> {
+        crate::output("")
+    }
+}
--- aoc/src/bin/aoc2022/mod.rs
+++ aoc/src/bin/aoc2022/mod.rs
@@ -5,12 +5,14 @@ mod aoc2022_02;
 mod aoc2022_03;
 mod aoc2022_04;
 mod aoc2022_05;
+mod aoc2022_06;
 
 use aoc2022_01::*;
 use aoc2022_02::*;
 use aoc2022_03::*;
 use aoc2022_04::*;
 use aoc2022_05::*;
+use aoc2022_06::*;
 
 pub fn run_2022(which: Selector) {
     let mut day_01 = Aoc2022_01::new();
@@ -18,9 +20,11 @@ pub fn run_2022(which: Selector) {
     let mut day_03 = Aoc2022_03::new();
     let mut day_04 = Aoc2022_04::new();
     let mut day_05 = Aoc2022_05::new();
+    let mut day_06 = Aoc2022_06::new();
 
     let mut days: Vec<&mut dyn Solution> = vec![
-        &mut day_01, &mut day_02, &mut day_03, &mut day_04, &mut day_05
+        &mut day_01, &mut day_02, &mut day_03, &mut day_04, &mut day_05,
+        &mut day_06
     ];
 
     match which {
