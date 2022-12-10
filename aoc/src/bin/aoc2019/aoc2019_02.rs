217f1526421180a9d146777eba853d00f9f793eb
TobiasAichinger <aichinger.tobias13@gmail.com>
2022-12-10 13:31:11 +0100

    2019 day 2 (stable): made solve function

--- aoc/src/bin/aoc2019/aoc2019_02.rs
+++ aoc/src/bin/aoc2019/aoc2019_02.rs
@@ -8,6 +8,26 @@ impl Aoc2019_02 {
     pub fn new() -> Self {
         Self { d: vec![] }
     }
+
+    fn solve(data: &mut Vec<usize>) -> usize {
+        let mut i: usize = 0;
+
+        loop {
+            let inst = data[i];
+            let f: usize = data[i + 1];
+            let s: usize = data[i + 2];
+            let t: usize = data[i + 3];
+
+            match inst {
+                1 => data[t] = data[f] + data[s],
+                2 => data[t] = data[f] * data[s],
+                99 => break,
+                _ => panic!("invalid instruction")
+            }
+            i+=4;
+        } 
+        data[0]   
+    }
 }
 
 impl crate::Solution for Aoc2019_02 {
@@ -20,53 +40,22 @@ impl crate::Solution for Aoc2019_02 {
     }
 
     fn part1(&mut self) -> Vec<String> {
-        let mut i: usize = 0;
-
-        loop {
-            let inst = self.d[i];
-            let f: usize = self.d[i + 1];
-            let s: usize = self.d[i + 2];
-            let t: usize = self.d[i + 3];
-
-            match inst {
-                1 => self.d[t] = self.d[f] + self.d[s],
-                2 => self.d[t] = self.d[f] * self.d[s],
-                99 => break,
-                _ => panic!("invalid instruction")
-            }
-            i+=4;
-        } 
-        crate::output(self.d[0])
+        let mut cl: Vec<usize> = self.d.clone();
+        cl[1] = 12;
+        cl[2] = 2;
+        crate::output(Aoc2019_02::solve(&mut cl))
     }
 
     fn part2(&mut self) -> Vec<String> {
-        let mut i: usize = 0;
-        self.d = numbers("input/2019/02.txt", ",")[0].clone();
-
         for n in 0..100 {
             for v in 0..100 {
                 let mut c = self.d.clone();
                 c[1] = n;
                 c[2] = v;
 
-                loop {
-                    let inst = c[i];
-                    let f: usize = c[i + 1];
-                    let s: usize = c[i + 2];
-                    let t: usize = c[i + 3];
-        
-                    match inst {
-                        1 => c[t] = c[f] + c[s],
-                        2 => c[t] = c[f] * c[s],
-                        99 => break,
-                        _ => panic!("invalid instruction {inst}")
-                    }
-                    i+=4;
-                }
-                if c[0] == 19690720 {
+                if Aoc2019_02::solve(&mut c) == 19690720 {
                     return crate::output(100 * n + v);
                 }
-                i = 0;
             }
         }
 
