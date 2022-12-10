75ad42c80446f8352a65ccf570d25f6215c68e90
TobiasAichinger <aichinger.tobias13@gmail.com>
2022-12-10 22:10:37 +0100

    2022 day 9 (stable): both parts in one function

--- aoc/src/bin/aoc2022/aoc2022_09.rs
+++ aoc/src/bin/aoc2022/aoc2022_09.rs
@@ -1,25 +1,42 @@
 use std::collections::HashSet;
 
 pub struct Aoc2022_09 {
-    d: Vec<(char, i32)>
+    instr: Vec<(char, i32)>
 }
         
 impl Aoc2022_09 {
     pub fn new() -> Self {
-        Self { d: Vec::new() }
+        Self { instr: Vec::new() }
     }
 
-    /*
-        x:4 y:5 h
-        x:3 y:5 t
-
-        x: // 
-    */
     fn simulate(&self, knots: &mut Vec<(i32, i32)>, p1: usize, p2: usize) {
         if knots[p1].1.abs_diff(knots[p2].1) > 1 || knots[p1].0.abs_diff(knots[p2].0) > 1 {
             knots[p2].1 += (knots[p1].1 - knots[p2].1).signum();
             knots[p2].0 += (knots[p1].0 - knots[p2].0).signum();
         }
+    }
+
+    fn solve(&self, part: bool) -> usize {
+        let mut positions: HashSet<(i32, i32)> = HashSet::from([(0,0)]);
+        let mut knots: Vec<(i32, i32)> = if part { vec![(0,0);2] } else { vec![(0,0);10] };
+
+        for i in 0..self.instr.len() {
+            for _ in 0..self.instr[i].1 {
+                match self.instr[i].0 {
+                    'R' => knots[0].0 += 1,
+                    'L' => knots[0].0 -= 1,
+                    'U' => knots[0].1 += 1,
+                    'D' => knots[0].1 -= 1,
+                    _ => panic!()
+                };
+                for j in 1..knots.len() {
+                    self.simulate(&mut knots, j-1, j);
+                }
+                positions.insert(knots[knots.len()-1]);
+            }
+        }
+
+        positions.len()
     }
 }
         
@@ -29,57 +46,16 @@ impl crate::Solution for Aoc2022_09 {
     }
         
     fn parse(&mut self) {
-        let input: String = std::fs::read_to_string("input/2022/09.txt").expect("unable to open file!");
-        
-        for line in input.lines() {
-            let s: Vec<_> = line.split(' ').collect();
-
-            self.d.push((s[0].chars().next().unwrap(), s[1].parse::<i32>().unwrap()));
+        for line in aoc::read_to_slice("input/2022/09.txt", " ") {
+            self.instr.push((line[0].chars().next().unwrap(), line[1].parse::<i32>().unwrap()));
         }
     }
 
     fn part1(&mut self) -> Vec<String> {
-        let mut positions: HashSet<(i32, i32)> = HashSet::new();
-        let mut knots: Vec<(i32, i32)> = vec![(0,0);2];
-
-        for i in 0..self.d.len() {
-            for _ in 0..self.d[i].1 {
-                positions.insert(knots[knots.len()-1]);
-                match self.d[i].0 {
-                    'R' => knots[0].0 += 1,
-                    'L' => knots[0].0 -= 1,
-                    'U' => knots[0].1 += 1,
-                    'D' => knots[0].1 -= 1,
-                    _ => panic!()
-                };
-                self.simulate(&mut knots, 0, 1);
-            }
-        }
-
-        crate::output(positions.len())
+        crate::output(self.solve(true))
     }
 
     fn part2(&mut self) -> Vec<String> {
-        let mut positions: HashSet<(i32, i32)> = HashSet::new();
-        let mut knots: Vec<(i32, i32)> = vec![(0,0);10];
-        positions.insert(knots[knots.len()-1]);
-
-        for i in 0..self.d.len() {
-            for _ in 0..self.d[i].1 {
-                match self.d[i].0 {
-                    'R' => knots[0].0 += 1,
-                    'L' => knots[0].0 -= 1,
-                    'U' => knots[0].1 += 1,
-                    'D' => knots[0].1 -= 1,
-                    _ => panic!()
-                };
-                for j in 0..knots.len()-1 {
-                    self.simulate(&mut knots, j, j+1);
-                }
-                positions.insert(knots[knots.len()-1]);
-            }
-        }
-
-        crate::output(positions.len())
+        crate::output(self.solve(false))
     }
 }
