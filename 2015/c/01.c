209870e553489fbcb0146647076af6952f92c510
TobiasAichinger <aichinger.tobias13@gmail.com>
2023-01-01 23:09:00 +0100

    C 2015 (stable): day 1

---
+++ 2015/c/01.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	FILE *fp = fopen("../input/01", "r");
+	char ch;
+	int loc = 0;
+	int part = 1;
+	int iter = 0;
+
+	while ((ch = fgetc(fp)) != EOF) {
+		if (part) {
+			iter += 1;
+		}
+
+		if (ch == '(') {
+			loc += 1;
+		} else if (ch == ')') {
+			loc -= 1;
+		}
+
+		if (part && loc < 0) {
+			part = 0;
+		}
+	}
+
+	printf("Part 1: %i\n", loc);
+	printf("Part 2: %i\n", iter);
+
+	fclose(fp);
+	return 0;
+}
