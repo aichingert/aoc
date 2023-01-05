e85fef61d0a4b109af3e39ade40e1124b524f9ed
TobiasAichinger <aichinger.tobias13@gmail.com>
2023-01-05 13:47:08 +0100

    2015 (stable): simplified day 6

--- 2015/python/06.py
+++ 2015/python/06.py
@@ -1,37 +1,10 @@
 # Advent of Code 2015, day 6
 # (c) aichinger
 
-def part1(com):
-    m = [[False for _ in range(1000)] for _ in range(1000)]
-
-    for l in com:
-        vls = l.split(" ")
-        vls = vls if vls[0] != "toggle" else ["work"] + vls
-        fx,fy = vls[2].split(",",1)
-        tx,ty = vls[4].split(",",1)
-
-        for y in range(int(fy),int(ty)+1):
-            for x in range(int(fx),int(tx)+1):
-                if vls[0] != "turn":
-                    m[y][x] = not m[y][x]
-                else:
-                    if vls[1] == "on":
-                        m[y][x] = True
-                    else:
-                        m[y][x] = False
-
-    s = 0
-    for i in range(len(m)):
-        for j in range(len(m[i])):
-            if m[i][j]:
-                s += 1
-
-    return s
-
-def part2(com):
+def solve(instr, part):
     m = [[0 for _ in range(1000)] for _ in range(1000)]
 
-    for l in com:
+    for l in instr:
         vls = l.split(" ")
         vls = vls if vls[0] != "toggle" else ["work"] + vls
         fx,fy = vls[2].split(",",1)
@@ -39,17 +12,18 @@ def part2(com):
 
         for y in range(int(fy),int(ty)+1):
             for x in range(int(fx),int(tx)+1):
-                if vls[0] != "turn":
-                    m[y][x] += 2
+                if vls[1] == "on":
+                    if part: m[y][x] = 1
+                    else: m[y][x] += 1
+                elif vls[1] == "off":
+                    if part: m[y][x] = 0
+                    else: m[y][x] = max(0, m[y][x]-1)
                 else:
-                    if vls[1] == "on":
-                        m[y][x] += 1
-                    else:
-                        m[y][x] = max(0, m[y][x] - 1)
+                    if part: m[y][x] = 1 - m[y][x]
+                    else: m[y][x] += 2
     return sum(sum(m, []))
 
 inp = open("../input/06").read().split("\n")
 
-print("Part 1:",part1(inp))
-print("Part 2:",part2(inp))
-
+print("Part 1:",solve(inp, True))
+print("Part 2:",solve(inp, False))
--- 2015/rust/06.rs
+++ 2015/rust/06.rs
@@ -1,44 +1,20 @@
 // Advent of Code 2015, day 6
 // (c) aichingert
 
-fn part1(inp: &str) -> u32 {
-    let mut m: [[bool;1000];1000] = [[false;1000];1000];
-
-    for l in inp.lines() {
-        let (vls,fx,fy,tx,ty) = parse(l);
-        for y in fy..=ty {
-            for x in fx..=tx {
-                if vls[0] != "turn" {
-                    m[y][x] = !m[y][x];
-                } else {
-                    if vls[1] == "on" {
-                        m[y][x] = true;
-                    } else{
-                        m[y][x] = false;
-                    }
-                }
-            }
-        }
-    }
-
-    m.iter().map(|l| l.iter().map(|val| if *val { 1 } else { 0 }).sum::<u32>()).sum::<u32>()
-}
-
-fn part2(inp: &str) -> u32 {
+fn solve(inp: &str, part: bool) -> u32 {
     let mut m: [[i8;1000];1000] = [[0;1000];1000];
 
     for l in inp.lines() {
         let (vls,fx,fy,tx,ty) = parse(l);
+
         for y in fy..=ty {
             for x in fx..=tx {
-                if vls[0] != "turn" {
-                    m[y][x] += 2;
+                if vls == "on" {
+                    if part { m[y][x] = 1 } else { m[y][x] += 1 }
+                } else if vls == "off" {
+                    if part { m[y][x] = 0 } else { m[y][x] = std::cmp::max(0, m[y][x]-1) }
                 } else {
-                    if vls[1] == "on" {
-                        m[y][x] += 1;
-                    } else{
-                        m[y][x] = std::cmp::max(0, m[y][x]-1);
-                    }
+                    if part { m[y][x] = 1 - m[y][x] } else { m[y][x] += 2 }
                 }
             }
         }
@@ -47,19 +23,19 @@ fn part2(inp: &str) -> u32 {
     m.iter().map(|l| l.iter().map(|val| *val as u32).sum::<u32>()).sum::<u32>()
 }
 
-fn parse(line: &str) -> (Vec<&str>,usize,usize,usize,usize) {
+fn parse(line: &str) -> (&str,usize,usize,usize,usize) {
     let mut vls: Vec<&str> = line.split(' ').collect();
     if vls.len() == 4 { vls.insert(0,"work"); }
     let (fx,fy): (&str,&str) = vls[2].split_once(',').unwrap();
     let (fx,fy): (usize, usize) = (fx.parse().unwrap(), fy.parse().unwrap());
     let (tx,ty): (&str,&str) = vls[4].split_once(',').unwrap();
     let (tx,ty): (usize, usize) = (tx.parse().unwrap(), ty.parse().unwrap());
-    (vls,fx,fy,tx,ty)
+    (vls[1],fx,fy,tx,ty)
 }
  
 fn main() {
     let inp = std::fs::read_to_string("../input/06").unwrap();
 
-    println!("Part 1: {}", part1(&inp));
-    println!("Part 2: {}", part2(&inp));
+    println!("Part 1: {}", solve(&inp, true));
+    println!("Part 2: {}", solve(&inp, false));
 }
