3bf87b32b33625e239b6ccbeda6287c7b536cfc5
TobiasAichinger <aichinger.tobias13@gmail.com>
2023-01-21 15:44:50 +0100

    Rust Utils (stable): added permutations

---
+++ utils/rust/permutations.rs
@@ -0,0 +1,21 @@
+// Permutations
+
+use std::fmt::Display;
+
+// [A,B,C] -> [A,B,C], [B,A,C], [C,A,B], [B,C,A], [C,B,A], [A,C,B]
+pub fn permutations<T: Display + Clone>(size: usize, vec: &mut Vec<T>, perms: &mut Vec<Vec<T>>) {
+    if size == 1 {
+        perms.push(vec.clone());
+    } else {
+        permutations(size - 1, vec, perms);
+
+        for i in 0..size-1 {
+            if i & 1 == 0 {
+                vec.swap(i, size-1)
+            } else {
+                vec.swap(0, size-1)
+            }
+            permutations(size-1, vec, perms);
+        }
+    }
+}
