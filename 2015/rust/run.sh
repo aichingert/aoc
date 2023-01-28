b8fadf4e6a965cec3bf4535ff7c2bb131a65eb28
aichingert <aichinger.tobias13@gmail.com>
2023-01-28 12:00:18 +0100

    Linux (stable): updated run.sh to work on win and linux

--- 2015/rust/run.sh
+++ 2015/rust/run.sh
@@ -1,3 +1,9 @@
-rustc $1.rs && ./$1.exe
-rm $1.exe
-rm $1.pdb
+if test "$OSTYPE" == "linux-gnu" ; then
+	exe="$(basename $1 .rs)"
+	rustc $1 && ./$exe
+	rm $exe
+else
+	rustc $1.rs && ./$1.exe
+	rm $1.exe
+	rm $1.pdb
+fi
