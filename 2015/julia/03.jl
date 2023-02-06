dec92f76b6ee387f2d29c4b753b0294f6712e6dd
aichingert <aichinger.tobias13@gmail.com>
2023-02-06 10:23:02 +0100

    Julia 2015 (stable): day 3

--- 2015/julia/03.jl
+++ 2015/julia/03.jl
@@ -3,25 +3,33 @@
 
 open("../input/03", "r") do f
 	loc = [[0,0],[0,0]]
+	pos = [0,0]
+	cur = 0
     p1 = Dict([0,0] => true)
-    p2 = 0
+    p2 = Dict([0,0] => true)
 
 	while !eof(f)
 		ch = read(f, Char)
 		
 		if ch == '^' 
-			loc[1][1] += 1
+			loc[cur+1][1] += 1
+			pos[1] += 1
 		elseif ch == 'v'
-			loc[1][1] -= 1
+			loc[cur+1][1] -= 1
+			pos[1] -= 1
 		elseif ch == '>'
-			loc[1][2] += 1
+			loc[cur+1][2] += 1
+			pos[2] += 1
 		else 
-			loc[1][2] -= 1
+			loc[cur+1][2] -= 1
+			pos[2] -= 1
 		end
 		
-		push!(p1, [loc[1][1],loc[1][2]] => true)
+		push!(p1, [pos[1],pos[2]] => true)
+		push!(p2, [loc[cur+1][1],loc[cur+1][2]] => true)
+		cur = 1 - cur
     end
 
     println("Part 1: " * string(length(p1)))
-    println("Part 2: " * string(p2))
+    println("Part 2: " * string(length(p2)))
 end
