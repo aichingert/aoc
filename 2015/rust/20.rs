53b6bc58c1e502fb87bd538c2ceb138fbf0a6b23
aichingert <aichinger.tobias13@gmail.com>
2023-02-10 17:25:42 +0100

    Rust 2015 (stable): optimized part 1

--- 2015/rust/20.rs
+++ 2015/rust/20.rs
@@ -1,25 +1,30 @@
 // Advent of Code 2015, day 20
 // (c) aichingert
 
-fn part1(inp: u32) -> u32 {
-    let mut ans = 1u32;
-
-    loop {
-        let mut cur = ans;
-
-        for i in 1..=ans / 2 {
-            if ans % i == 0 { cur += i; }
+fn fact(n: u32) -> u32 {
+    let mut fac = n+1;
+ 
+    for i in 2..=(n as f64).sqrt() as u32 {
+        if n % i == 0 {
+            fac += i;
+            if i*i != n { fac += n / i; }
         }
-
-        if cur * 10 >= inp {
-            return ans;
-        }
-
-        ans += 1;
     }
+
+    return fac
 }
 
-fn part2() {}
+fn part1(inp: u32) -> u32 {
+    let mut ans = 2;
+
+    while fact(ans) <= inp { ans += 1; }
+
+    ans
+}
+
+fn part2(inp: u32) ->  u32 {
+    0
+}
 
 fn main() {
     let inp = std::fs::read_to_string("../input/20").unwrap().trim().parse::<u32>().unwrap();
