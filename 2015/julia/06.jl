418d8549b21f36af7aa15218a17528b6b039d4f4
aichingert <aichinger.tobias13@gmail.com>
2023-02-11 13:08:14 +0100

    Julia 2015 (stable): day 6

--- 2015/julia/06.jl
+++ 2015/julia/06.jl
@@ -10,30 +10,21 @@ function parse(arg::SubString)
     return (parse(Int32, cords[1]), parse(Int32, cords[2]))
 end
 
-function part1(lines::Vector{String}, map::Matrix) 
+function solve(lines::Vector{String}, map::Matrix, part::Bool) 
     for line in lines
         vls = split(line, " ")
-        
-        if vls[1] == "toggle"
-            x,y = parse(vls[2])
-            cx,cy = parse(vls[4])
+        if length(vls) == 4 insert!(vls, 1, "") end
+        x,y = parse(vls[3])
+        cx,cy = parse(vls[5])
 
-            for i in y:cy
-                for j in x:cx
-                    map[i,j] = 1 - map[i,j]
-                end
-            end
-        else
-            x,y = parse(vls[3])
-            cx,cy = parse(vls[5])
-
-             for i in y:cy
-                for j in x:cx
-                    if vls[2] == "on"
-                        map[i,j] = 1
-                    else
-                        map[i,j] = 0
-                    end
+        for i in y:cy
+            for j in x:cx
+                if vls[2] == "on"
+                    if part map[i,j] = 1 else map[i,j] += 1 end
+                elseif vls[2] == "off"
+                    if part map[i,j] = 0 else map[i,j] = max(0, map[i,j] - 1)  end
+                else
+                    if part map[i,j] = 1 - map[i,j] else map[i,j] += 2 end
                 end
             end
         end
@@ -42,8 +33,10 @@ function part1(lines::Vector{String}, map::Matrix)
     return sum(map)
 end
 
+map = zeros(Int8,1000,1000)
 inp = readlines("../input/06")
-map = zeros(Int8, 1000, 1000)
-println(part1(inp,map))
+
+println("Part 1: " * string(solve(inp,copy(map), true)))
+println("Part 2: " * string(solve(inp,map, false)))
 
 end
