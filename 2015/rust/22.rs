04cbae9979c13f9fa99a54ae3008daac6b6f5327
aichingert <aichinger.tobias13@gmail.com>
2023-02-12 23:52:09 +0100

    Rust 2015 (stable): day 22 part 1

--- 2015/rust/22.rs
+++ 2015/rust/22.rs
@@ -1,7 +1,7 @@
 // Advent of Code 2015, day 22
 // (c) aichingert
 
-#[derive(Debug, Clone, Copy)]
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Ord, PartialOrd)]
 enum Spell {
     MagicMissile,
     Drain,
@@ -25,9 +25,22 @@ impl Spell {
         match self {
             Spell::MagicMissile => 1,
             Spell::Drain => 1,
-            Spell::Shield => 6,
+            Spell::Shield => 7,
             Spell::Poison => 6,
             Spell::Recharge => 5,
+        }
+    }
+
+    fn consume(&self, player: &mut Player, boss: &mut Boss) {
+        match self {
+            Spell::MagicMissile => { boss.hp -= 4; },
+            Spell::Drain => { 
+                player.hp += 2;
+                boss.hp -= 2;
+            },
+            Spell::Shield => { player.armor = 7; },
+            Spell::Poison => { boss.hp -= 3; },
+            Spell::Recharge => { player.mana += 101; },
         }
     }
 }
@@ -44,44 +57,6 @@ impl Effect {
     }
 }
 
-#[derive(Debug, Clone)]
-struct ActiveEffects {
-    effects: Vec<Effect>,
-}
-
-impl ActiveEffects {
-    fn new() -> Self {
-        Self { effects: Vec::new() }
-    }
-
-    fn update(&mut self, player: &mut Player, boss: &mut Boss) {
-        player.mana -= self.effects[0].spell.cost();
-
-        for effect in self.effects.iter() {
-            match effect.spell {
-                Spell::MagicMissile => boss.hp -= 4,
-                Spell::Drain => {
-                    player.hp += 2;
-                    boss.hp -= 2;
-                },
-                Spell::Shield => player.shield = 7,
-                Spell::Poison => boss.hp -= 3,
-                Spell::Recharge => player.mana += 101,
-            };
-        }
-
-        let mut i: usize = 0;
-        while i < self.effects.len() {
-            if self.effects[i].timer == 1 {
-                self.effects.remove(i);
-            } else {
-                self.effects[i].timer -= 1;
-                i += 1;
-            }
-        }
-    }
-}
-
 #[derive(Debug, Clone, Copy)]
 struct Boss {
     hp: i32,
@@ -92,51 +67,73 @@ struct Boss {
 struct Player {
     hp: i32,
     mana: i32,
-    shield: i32,
+    armor: i32,
 }
 
 const SPELLS: [Spell;5] = [Spell::MagicMissile, Spell::Drain, Spell::Shield, Spell::Poison, Spell::Recharge];
 
-fn part1(player: &mut Player, boss: &mut Boss) -> i32 {
-    let mut amt: i32 = i32::MAX;
-    let mut effects = ActiveEffects::new();
+fn update(player: &mut Player, boss: &mut Boss, effects: &mut Vec<Effect>) {
+    let mut i = 0;
 
-    for spell in SPELLS {
-        effects.effects.push(Effect::new(spell));
-        if let Some(mana) = simulation(&mut player.clone(), &mut boss.clone(), &mut effects) {
-            amt = amt.min(mana);
+    while i < effects.len() {
+        effects[i].spell.consume(player, boss);
+
+        if effects[i].timer == 1 {
+            let effect = effects.remove(i);
+            
+            if effect.spell == Spell::Shield {
+                player.armor = 0;
+            }
+        } else {
+            effects[i].timer -= 1;
+            i += 1;
         }
-        println!("{:?}, {:?}, {:?}", spell, amt, effects.effects);
-        effects.effects.clear();
     }
-
-    amt
 }
 
-fn simulation(player: &mut Player, boss: &mut Boss, real: &mut ActiveEffects) -> Option<i32> {
-    let mut m = 0;
-    real.update(player, boss);
-
-    if boss.hp <= 0 && player.mana >= 0 {
-        return Some(player.mana);
-    } else if player.mana <= 0 || player.hp <= 0 {
-        return None;
-    } 
-    
-    for spell in SPELLS {
-        let mut fake_p = player.clone();
-        let mut fake = real.clone();
-        fake_p.hp -= (boss.damage - player.shield).max(1);
-
-        fake.effects.push(Effect::new(spell));
-
-        if let Some(mana) = simulation(&mut fake_p, &mut boss.clone(), &mut fake) {
-            m = m.max(mana);
+fn contains_spell(effects: &Vec<Effect>, spell: &Spell) -> bool {
+    for i in 0..effects.len() {
+        if effects[i].spell == *spell {
+            return true;
         }
     }
 
+    false
+}
 
-    Some(m)
+fn part1(player: &Player, boss: &Boss, effects: &Vec<Effect>, cur: i32) -> Option<i32> {
+    let mut wins: Vec<i32> = Vec::new();
+
+    for spell in SPELLS.iter() {
+        let mut sim_player = player.clone();
+        let mut sim_boss = boss.clone();
+        let mut sim_effects = effects.clone();
+        
+        update(&mut sim_player, &mut sim_boss, &mut sim_effects);
+        
+        if sim_boss.hp <= 0 { return Some(cur); }
+        if sim_player.mana - spell.cost() < 0 || contains_spell(&sim_effects, spell) { continue; }
+
+        sim_player.mana -= spell.cost();
+        sim_effects.push(Effect::new(*spell));
+        
+        update(&mut sim_player, &mut sim_boss, &mut sim_effects);
+
+        if sim_boss.hp <= 0 { 
+            wins.push(cur + spell.cost());
+            continue;
+        }
+        
+        sim_player.hp -= (sim_boss.damage - sim_player.armor).max(1);
+
+        if sim_player.hp <= 0 { continue; }
+
+        if let Some(mana) = part1(&sim_player, &sim_boss, &sim_effects, cur + spell.cost()) {
+            wins.push(mana);
+        }
+    }
+
+    wins.into_iter().min()
 }
 
 fn part2() {}
@@ -147,13 +144,21 @@ fn parse() -> (Player, Boss) {
     let hp = inp[0].split(": ").collect::<Vec<&str>>();
     let dmg = inp[1].split(": ").collect::<Vec<&str>>();
 
-    (Player { hp: 50, mana: 500, shield: 0 }, Boss { hp: hp[1].parse::<i32>().unwrap(), damage: dmg[1].parse::<i32>().unwrap() })
+    (Player { hp: 50, mana: 500, armor: 0 }, Boss { hp: hp[1].parse::<i32>().unwrap(), damage: dmg[1].parse::<i32>().unwrap() })
 }
 
 fn main() {
     let (mut player, mut boss) = parse();
-    let (mut p, mut b): (Player, Boss) = (Player { hp: 10, shield: 0, mana: 250 }, Boss { hp: 13, damage: 8 });
+    let (mut p, mut b): (Player, Boss) = (Player { hp: 10, mana: 250, armor: 0 }, Boss { hp: 14, damage: 8 });
 
-    println!("Part 1: {:?}", part1(&mut p, &mut b));
+    /*
+    let mut e = vec![Effect::new(Spell::Shield), Effect::new(Spell::Poison)];
+    println!("{:?} - {:?} - {:?}", player.armor, boss.hp, e);
+    while e.len() > 0 {
+        update(&mut player, &mut boss, &mut e);
+        println!("{:?} - {:?} - {:?}", player.armor, boss.hp, e);
+    }
+    */
+    println!("Part 1: {:?}", part1(&player, &boss, &vec![], 0));
     //println!("Part 2: {}", part2());
 }
