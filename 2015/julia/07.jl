9aa1677a6dd78197b55cadedeb4e2e8643a08570
aichingert <aichinger.tobias13@gmail.com>
2023-02-16 10:11:56 +0100

    Julia 2015 (stable): day 7 part 2

--- 2015/julia/07.jl
+++ 2015/julia/07.jl
@@ -3,30 +3,24 @@
 
 module Day7
 
-function solve(instr::Dict{Any,Any}, calc::Dict{Any,Any}, cur::String)
-    try
-        res = parse(Int32, cur)
-        return res
-    catch end
+function solve(instr::Dict{Any,Any}, cur::String)
+    if tryparse(UInt16, cur) !== nothing
+	return parse(UInt16, cur)
+    end
 
     if cur in keys(calc) return calc[cur] end
 
     values = split(instr[cur], " ")
 
-    # println(values)
-    println(calc, cur)
-    if length(values) == 1 global calc[cur] = solve(instr,calc,String(values[1])) end
-    if length(values) == 2 global calc[cur] = ~solve(instr,calc,String(values[2])) 
+    if length(values) == 1 global calc[cur] = solve(instr,String(values[1])) end
+    if length(values) == 2 global calc[cur] = ~solve(instr,String(values[2])) 
     elseif length(values) == 3
-        if values[2] == "AND" 
-            global calc[cur] = solve(instr,calc,String(values[1])) & solve(instr,calc,String(values[3]))
-        end
-        if values[2] == "OR" global calc[cur] = solve(instr,calc,String(values[1])) | solve(instr,calc,String(values[3])) end
-        if values[2] == "LSHIFT" global calc[cur] = solve(instr,calc,String(values[1])) << solve(instr,calc,String(values[3]))
-        else global calc[cur] = solve(instr,calc,String(values[1])) >> solve(instr,calc,String(values[3])) end
+        if values[2] == "AND" global calc[cur] = solve(instr,String(values[1])) & solve(instr,String(values[3])) end
+        if values[2] == "OR" global calc[cur] = solve(instr,String(values[1])) | solve(instr,String(values[3])) end
+        if values[2] == "LSHIFT" global calc[cur] = solve(instr,String(values[1])) << solve(instr,String(values[3]))
+    	elseif values[2] == "RSHIFT" global calc[cur] = solve(instr,String(values[1])) >> solve(instr,String(values[3])) end
     end
 
-    println(calc, cur)
     return calc[cur] 
 end
 
@@ -38,8 +32,11 @@ for l in eachline("../input/07")
     instr[s[end]] = s[begin]
 end
 
-solve(instr, calc,"d")
-part1 = solve(instr, calc,"d")
-println(part1)
+part1 = solve(instr, "a")
+empty!(calc)
+calc["b"] = part1
+
+println("Part 1: " * string(part1))
+println("Part 2: " * string(solve(instr, "a")))
 
 end
