73245ddf12cd6155cc17a86415eb634d5c630db9
aichingert <aichinger.tobias13@gmail.com>
2023-02-19 16:10:28 +0100

    Haskell 2015 (stable): day 1 part 2

--- 2015/haskell/01.hs
+++ 2015/haskell/01.hs
@@ -5,16 +5,24 @@ module Main where
 
 import Data.Char
 
-part1 :: [Char] -> Int
-part1 [] = 0
-part1 (x:xs) = case x of
-  '(' -> part1 xs + 1
-  ')' -> part1 xs - 1
-  _ -> part1 xs
+part1 :: [Char] -> Int -> Int
+part1 [] loc = loc
+part1 (x:xs) loc | x == '(' = part1 xs (loc + 1)
+                 | x == ')' = part1 xs (loc - 1)
+
+part2 :: [Char] -> Int -> Int -> Int
+part2 [] loc cur | loc < 0 = cur 
+                 | otherwise = -1
+part2 (x:xs) loc cur | loc < 0 = cur
+                     | x == '(' = part2 xs (loc + 1) (cur + 1)
+                     | x == ')' = part2 xs (loc - 1) (cur + 1)
 
 main :: IO ()
 main = do
   input <- readFile "../input/01"
-  print $ part1 input
+  putStr "Part 1: "
+  print $ part1 input 0
+  putStr "Part 2: "
+  print $ part2 input 0 0
 
 
