c8276c5acf5638ccff69512884b198125e09b02d
aichingert <aichinger.tobias13@gmail.com>
2023-02-25 00:57:39 +0100

    Rust 2022 (stable): day 2

--- 2022/rust/02.rs
+++ 2022/rust/02.rs
@@ -1,20 +1,33 @@
 // Advent of Code 2022, day 2
 // (c) aichingert
 
-fn part1() {}
+fn part1(n: &Vec<(i32,i32)>) -> i32 {
+    n.iter().map(|(lhs,rhs)| rhs + (3 - (2 + lhs - rhs) % 3) % 3 * 3).sum::<i32>()
+}
 
-fn part2() {}
+fn part2(n: &Vec<(i32,i32)>) -> i32 {
+    n.iter().map(|(lhs,rhs)| (lhs+rhs) % 3 + 1 + (rhs - 1) * 3).sum::<i32>()
+}
 
-fn parse() -> Vec<(RPS, RPS)> {
-    let mut out = Vec::<(RPS,RPS)>::new();
+fn parse() -> Vec<(i32,i32)> {
+    std::fs::read_to_string("../input/02").unwrap().trim().lines().map(|l| {
+        let (lhs,rhs) = l.split_once(' ').unwrap();
+        (map_value(lhs),map_value(rhs))
+    }).collect::<Vec<(i32,i32)>>()
+}
 
-    for l in std::fs::read_to_string("../input/01").unwrap().lines() {
-        let val = l.split(' ').collect::<Vec<&str
+fn map_value(val: &str) -> i32 {
+    match val {
+        "A" | "X" => 1,
+        "B" | "Y" => 2,
+        "C" | "Z" => 3,
+        _ => panic!("Invalid input!"),
     }
 }
 
 fn main() {
+    let n = parse();
 
-    println!("Part 1: {}", part1());
-    println!("Part 2: {}", part2());
+    println!("Part 1: {}", part1(&n));
+    println!("Part 2: {}", part2(&n));
 }
