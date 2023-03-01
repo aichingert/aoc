e31f9187f0cb19d2c573a32265d661bf791d312e
aichingert <aichinger.tobias13@gmail.com>
2023-03-01 17:34:25 +0100

    Rust 2016 (stable): day 23 part 2

--- 2016/rust/23.rs
+++ 2016/rust/23.rs
@@ -10,11 +10,15 @@ fn part1(inp: &String) -> i128 {
     runner.exec(&String::from("a"))
 }
 
-fn part2() {}
+fn part2(inp: &String) -> i128 {
+    let mut runner = Runner::new(inp);
+    *runner.reg.get_mut(&String::from("a")).unwrap() = 12;
+    runner.exec(&String::from("a"))
+}
 
 fn main() {
     let inp = std::fs::read_to_string("../input/23").unwrap();
 
     println!("Part 1: {}", part1(&inp));
-    //println!("Part 2: {}", part2());
+    println!("Part 2: {}", part2(&inp));
 }
