1b9da370f04f057c14258e59ca4324a01b855463
aichingert <aichinger.tobias13@gmail.com>
2023-03-28 21:57:17 +0200

    Rust 2016 (stable): day 14

--- 2016/rust/14.rs
+++ 2016/rust/14.rs
@@ -3,34 +3,45 @@
 
 #[path="../../utils/rust/md5.rs"] mod md5;
 
-fn part1(salt: &str) -> usize {
+fn generate_hash(salt: &str, streching: bool) -> Vec<char> {
+    match streching {
+        true => {
+            let mut hash = md5::md5_utf8(salt);
+
+            for i in 0..2016 { hash = md5::md5_utf8(&hash); }
+            hash.chars().collect::<Vec<char>>()
+        },
+        false => md5::md5_utf8(salt).chars().collect::<Vec<char>>()
+    }
+}
+
+fn solve(salt: &str, streching: bool) -> usize {
     let mut index = 0;
     let mut count = 0;
     let mut hashes = Vec::<Vec<char>>::new();
 
     while hashes.len() < 1000 {
-        hashes.push(md5::md5_utf8(&format!("{salt}{index}")).chars().collect::<Vec<char>>());
+        hashes.push(generate_hash(&format!("{salt}{index}"), streching));
         index += 1;
     }
 
     'cond: while count < 64 {
         let hash = hashes.remove(0);
+        hashes.push(generate_hash(&format!("{salt}{index}"), streching));
         let mut fst: char = '_';
-        hashes.push(md5::md5_utf8(&(salt.to_owned() + &index.to_string())).chars().collect::<Vec<char>>());
         index += 1;
 
         for i in 0..hash.len()-2 {
             if hash[i] == hash[i+1] && hash[i+1] == hash[i+2] {
-                fst = hash[i];
-                break;
+                fst = hash[i]; break;
             }
         }
 
         if fst != '_' {
             for i in 0..hashes.len() {
-                let t = vec![fst;5];
+                let vec = vec![fst;5];
                 for j in 0..hashes[i].len() - 5 {
-                    if &hashes[i][j..j+5] ==  t {
+                    if &hashes[i][j..j+5] == vec {
                         count += 1;
                         continue 'cond;
                     }
@@ -38,13 +49,13 @@ fn part1(salt: &str) -> usize {
             }
         }
     }
+
     index - 1001
 }
-fn part2() {}
 
 fn main() {
     let salt = std::fs::read_to_string("../input/14").unwrap();
 
-    println!("Part 1: {}", part1(&salt.trim()));
-    //println!("Part 2: {}", part2());
+    println!("Part 1: {}", solve(&salt.trim(), false));
+    println!("Part 2: {}", solve(&salt.trim(), true));
 }
