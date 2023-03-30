9b24bf60ac3e8bcd511ac3468ae805e402475946
aichingert <aichinger.tobias13@gmail.com>
2023-03-30 11:07:53 +0200

    Rust 2016 (stable): day 19 added math tricks

--- 2016/rust/19.rs
+++ 2016/rust/19.rs
@@ -1,35 +1,23 @@
 // Advent of Code 2016, day 19
 // (c) aichingert
 
-use std::collections::VecDeque;
-
-fn part1(inp: &VecDeque<u32>) -> u32 {
-    let mut clone = inp.clone();
-
-    while clone.len() > 1 {
-        clone.remove(1);
-        let first = clone.remove(0).unwrap();
-        clone.push_back(first);
-    }
-
-    clone[0]
+fn part1(inp: u32) -> u32 {
+    ((inp - (inp.next_power_of_two() >> 1)) * 2) % inp + 1
 }
 
-fn part2(inp: &VecDeque<u32>) -> u32 {
-    let mut clone = inp.clone();
+fn part2(inp: u32) -> u32 {
+    let mut survivor: u32 = 1;
+    while survivor * 3 < inp { survivor *= 3; }
 
-    while clone.len() > 1 {
-        clone.remove(clone.len()/2);
-        let first = clone.remove(0).unwrap();
-        clone.push_back(first);
+    match inp < 2 * survivor {
+        true => inp - survivor,
+        false => 2*inp - 3*survivor
     }
-
-    clone[0]
 }
 
 fn main() {
-    let inp = (1..=std::fs::read_to_string("../input/19").unwrap().trim().parse::<u32>().unwrap()).collect::<VecDeque<u32>>();
+    let inp = std::fs::read_to_string("../input/19").unwrap().trim().parse::<u32>().unwrap();
 
-    println!("Part 1: {}", part1(&inp));
-    println!("Part 2: {}", part2(&inp));
+    println!("Part 1: {}", part1(inp));
+    println!("Part 2: {}", part2(inp));
 }
