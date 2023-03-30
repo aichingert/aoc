0eae4f9fe1ddc5c64509081ab0d5f9129d1b8252
aichingert <aichinger.tobias13@gmail.com>
2023-03-31 00:02:06 +0200

    Rust 2016 (stable): day 18

---
+++ 2016/rust/18.rs
@@ -0,0 +1,28 @@
+// Advent of Code 2016, day 18
+// (c) aichingert
+
+fn solve(start: &mut Vec<Vec<char>>, steps: usize) -> usize {
+    for gen in start.len()-1..steps-1 {
+        start.push(vec![]);
+        for loc in 0..start[gen].len() {
+            let left = loc>0 && start[gen][loc-1] == '^';
+            let right = loc+1<start[gen].len() && start[gen][loc+1] == '^';
+
+            if start[gen][loc] == '^' && ((left && !right) || (!left && right))
+            || start[gen][loc] != '^' && ((left && !right) || (!left && right)){
+                start[gen+1].push('^');
+            } else {
+                start[gen+1].push('.');
+            }
+        }
+    }
+
+    start.iter().map(|vec| vec.iter().filter(|&trap| *trap != '^').count()).sum::<usize>()
+}
+
+fn main() {
+    let mut inp = vec![std::fs::read_to_string("../input/18").unwrap().trim().chars().collect::<Vec<char>>()];
+
+    println!("Part 1: {}", solve(&mut inp, 40));
+    println!("Part 2: {}", solve(&mut inp, 400000));
+}
