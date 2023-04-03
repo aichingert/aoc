eddf68bc9460a4e4f7017490cd259408d9b86420
aichingert <aichinger.tobias13@gmail.com>
2023-04-03 11:56:52 +0200

    Rust 2017 (stable): day 24

--- 2017/rust/24.rs
+++ 2017/rust/24.rs
@@ -1,14 +1,13 @@
 // Advent of Code 2017, day 24
 // (c) aichingert
 
-use std::collections::HashSet;
-
-#[derive(Copy,Clone,Hash)]
+#[derive(Clone,Debug)]
 struct Component {
     ports: [u32;2],
     used: [bool;2]
 }
 
+#[derive(Clone,Debug)]
 struct Bridge {
     connections: Vec<Component>,
 }
@@ -62,20 +61,46 @@ fn part1(comps: &mut Vec<Component>) -> u32 {
         }
     }
 
+    for start in starting {
+        let mut cur: Bridge = Bridge::new();
+        cur.add(start.clone());
+        rec_find(comps, &mut cur, &mut possible);
+    }
+    
+    let mut ans = 0;
 
-    0
+    for pos in possible {
+        ans = ans.max(pos.strength());
+    }
+
+    ans
 }
 
-// fn rec_find(comps: &mut Vec<Component>, cur: &mut bridge)
+fn rec_find(comps: &mut Vec<Component>, cur: &mut Bridge, bridges: &mut Vec<Bridge>) {
+    bridges.push(cur.clone());
+
+    let mut i: usize = 0;
+
+    while i < comps.len() {
+        let mut clone = cur.clone();
+        if clone.add(comps[i].clone()) {
+            let comp = comps.remove(i);
+            rec_find(&mut comps.clone(), &mut clone, bridges);
+            comps.insert(i, comp);
+        }
+
+        i += 1;
+    }
+}
 
 fn part2() {}
 
 fn main() {
-    let inp = std::fs::read_to_string("../input/24").unwrap()
+    let mut inp = std::fs::read_to_string("../input/24").unwrap()
         .lines()
         .map(|l| {let p = l.split_once('/').unwrap(); Component::new([p.0.parse::<u32>().unwrap(),p.1.parse::<u32>().unwrap()])})
         .collect::<Vec<Component>>();
 
-    println!("Part 1: {}", part1(&inp));
+    println!("Part 1: {}", part1(&mut inp));
     //println!("Part 2: {}", part2());
 }
