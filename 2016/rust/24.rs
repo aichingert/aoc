3776265898d7dfbb35125cd189272694bad95002
aichingert <aichinger.tobias13@gmail.com>
2023-04-04 15:38:46 +0200

    Rust 2016 (stable): day 24 part 1

--- 2016/rust/24.rs
+++ 2016/rust/24.rs
@@ -7,6 +7,7 @@ use std::collections::HashMap;
 fn part1(map: &Vec<Vec<char>>) -> u32 {
     let mut steps: u32 = u32::MAX;
     let mut positions: HashMap<char, (usize,usize)> = HashMap::new();
+    let mut dists: HashMap<(char,char), u32> = HashMap::new();
 
     for i in 0..map.len() {
         for j in 0..map[i].len() {
@@ -18,66 +19,60 @@ fn part1(map: &Vec<Vec<char>>) -> u32 {
 
     let mut vec = positions.keys().map(|&ch| ch).filter(|&ch| ch != '0').collect::<Vec<char>>();
     let mut perms = permutations::permutations(vec.len(), &mut vec);
-    let mut i = 0;
 
     for perm in perms.iter_mut() {
-        println!("{i}");
         perm.insert(0, '0');
-        steps = steps.min(find_dist(perm, &positions, map));
-        i += 1;
+        let mut s = 0;
+
+        for i in 0..perm.len()-1 {
+            if !dists.contains_key(&(perm[i],perm[i+1])) {
+                let dist = find_dist(perm[i], perm[i+1], &positions, map);
+                dists.insert((perm[i],perm[i+1]), dist);
+                dists.insert((perm[i+1],perm[i]), dist);
+            }
+
+            s += dists[&(perm[i], perm[i+1])];
+        }
+
+        steps = steps.min(s);
     }
 
     steps
 }
 
-fn find_dist(points: &Vec<char>, positions: &HashMap<char, (usize,usize)>, map: &Vec<Vec<char>>) -> u32 {
-    let mut dist = 0;
+fn find_dist(start: char, end: char, positions: &HashMap<char, (usize,usize)>, map: &Vec<Vec<char>>) -> u32 {
+    let from = positions[&start];
+    let to = positions[&end];
+    let mut dists: HashMap<(usize,usize), u32> = HashMap::from([(from,0)]);
+    let mut queue = vec![(from.0-1,from.1),(from.0+1,from.1),(from.0,from.1-1),(from.0,from.1+1)];
 
-    for i in 0..points.len()-1 {
-        let from = positions[&points[i]];
-        let to = positions[&points[i+1]];
-        let mut path: HashMap<(usize,usize), u32> = HashMap::from([(from,0)]);
-        let mut queue = vec![
-            (from.0 - 1, from.1),
-            (from.0 + 1, from.1),
-            (from.0, from.1 - 1),
-            (from.0, from.1 + 1),
-        ];
-
-        while let Some((y,x)) = queue.pop() {
-            if map[y][x] == '#' {
-                continue;
-            }
-
-            let mut min = u32::MAX;
-            min = min.min(*path.entry((y + 1, x)).or_insert(u32::MAX));
-            min = min.min(*path.entry((y, x + 1)).or_insert(u32::MAX));
-            min = min.min(*path.entry((y - 1, x)).or_insert(u32::MAX));
-            min = min.min(*path.entry((y, x - 1)).or_insert(u32::MAX));
-            min += 1;
-
-            let cur: &mut u32 = path.entry((y,x)).or_insert(u32::MAX);
-            if min < *cur {
-                *cur = min;
-
-                queue.push((y-1,x));
-                queue.push((y+1,x));
-                queue.push((y,x-1));
-                queue.push((y,x+1));
-            }
-
-            if path.contains_key(&to) && path[&to] < u32::MAX {
-                break;
-            }
+    while let Some((y,x)) = queue.pop() {
+        if map[y][x] == '#' {
+            continue;
         }
 
-        dist += path.get(&to).unwrap();
+        let mut min = u32::MAX;
+        min = min.min(*dists.entry((y + 1, x)).or_insert(u32::MAX));
+        min = min.min(*dists.entry((y, x + 1)).or_insert(u32::MAX));
+        min = min.min(*dists.entry((y - 1, x)).or_insert(u32::MAX));
+        min = min.min(*dists.entry((y, x - 1)).or_insert(u32::MAX));
+        min += 1;
+
+        let cur: &mut u32 = dists.entry((y,x)).or_insert(u32::MAX);
+        if min < *cur {
+            *cur = min;
+
+            queue.push((y-1,x));
+            queue.push((y+1,x));
+            queue.push((y,x-1));
+            queue.push((y,x+1));
+        }
     }
 
-    dist
+    *dists.get(&to).unwrap()
 }
 
-fn part2() {}
+//fn part2() {}
 
 fn main() {
     let inp = std::fs::read_to_string("../input/24").unwrap().trim()
