58cf0d2c6a3f0bd75ebbf36b96fe26857f0a3021
aichingert <aichinger.tobias13@gmail.com>
2023-04-09 13:54:15 +0200

    Rust 2017 (stable): day 11

--- 2017/rust/11.rs
+++ 2017/rust/11.rs
@@ -1,7 +1,8 @@
 // Advent of Code 2017, day 11
 // (c) aichingert
 
-fn solve(dirs: &Vec<&str>) -> u32 {
+fn solve(dirs: &Vec<&str>) -> (u32,u32) {
+    let mut furthest = 0;
     let mut loc = (0.0f32,0.0f32);
 
     for dir in dirs {
@@ -13,10 +14,12 @@ fn solve(dirs: &Vec<&str>) -> u32 {
             "sw" => { loc.0 -= 0.5; loc.1 -= 0.5; }
             "se" => { loc.0 -= 0.5; loc.1 += 0.5; }
             _ => {},
-        }
+        };
+
+        furthest = furthest.max((loc.0.abs() + loc.1.abs()) as u32);
     }
 
-    (loc.0.abs() + loc.1.abs()) as u32
+    ((loc.0.abs() + loc.1.abs()) as u32, furthest)
 }
 
 fn main() {
@@ -25,6 +28,8 @@ fn main() {
         .split(',')
         .map(|s| s.trim())
         .collect::<Vec<&str>>();
+    let (part1, part2) = solve(&inp);
 
-    println!("Part 1: {}", solve(&inp));
+    println!("Part 1: {}", part1);
+    println!("Part 2: {}", part2);
 }
