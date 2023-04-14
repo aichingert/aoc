86c21bdbf5d2578d7a8aeebbab140db42d43fbcd
aichingert <aichinger.tobias13@gmail.com>
2023-04-14 08:13:47 +0200

    Rust 2017 (stable): extracted knot_hash from day 10

--- 2017/rust/10.rs
+++ 2017/rust/10.rs
@@ -1,48 +1,8 @@
 // Advent of Code 2017, day 10
 // (c) aichingert
 
-const SIZE: usize = 256;
-
-fn hashing(list: &mut Vec<u32>, lengths: &Vec<usize>, skip_size: &mut usize, loc: &mut usize) -> u32 {
-    for i in 0..lengths.len() {
-        let mut col = (0..lengths[i]).map(|idx| list[(*loc + idx) % SIZE]).collect::<Vec<u32>>();
-        col = col.iter().rev().map(|n| *n).collect::<Vec<u32>>();
-
-        for j in 0..lengths[i] {
-            list[(*loc + j) % SIZE] = col[j];
-        }
-
-        *loc = (*loc + lengths[i] + *skip_size) % SIZE;
-        *skip_size += 1;
-    }
-
-    list[0] * list[1]
-}
-
-fn part2(hash: &str) -> String {
-    let mut lengths = hash.as_bytes().iter().map(|n| *n as usize).collect::<Vec<usize>>();
-    lengths.extend([17, 31, 73, 47, 23]);
-    let (mut skip_size, mut loc) = (0usize,0usize);
-    let mut sparse_hash = (0..SIZE as u32).collect::<Vec<u32>>();
-    let mut dense_hash = vec![0;16];
-    let mut knot_hash = String::new();
-
-    for _ in 0..64 {
-        hashing(&mut sparse_hash, &lengths, &mut skip_size, &mut loc);
-    }
-
-    for i in 0..16 {
-        for j in 0..16 {
-            dense_hash[i] ^= sparse_hash[i*16+j];
-        }
-    }
-
-    for i in 0..dense_hash.len() {
-        knot_hash.push_str(&format!("{:#04x}", dense_hash[i])[2..]);
-    }
-
-    knot_hash
-}
+#[path="./knot_hash.rs"] mod knot_hash;
+use knot_hash::SIZE;
 
 fn main() {
     let inp = std::fs::read_to_string("../input/10").unwrap();
@@ -51,6 +11,6 @@ fn main() {
         .map(|n| n.trim().parse().unwrap())
         .collect();
 
-    println!("Part 1: {}", hashing(&mut (0..SIZE as u32).collect::<Vec<u32>>(), &lengths, &mut 0, &mut 0));
-    println!("Part 2: {}", part2(&inp.trim()));
+    println!("Part 1: {}", knot_hash::shuffling(&mut (0..SIZE as u32).collect::<Vec<u32>>(), &lengths, &mut 0, &mut 0));
+    println!("Part 2: {}", knot_hash::knot_hash(&inp.trim()));
 }
