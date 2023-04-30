881bd1ee8681afec5af6f561257e2349b45ecbb6
aichingert <aichinger.tobias13@gmail.com>
2023-04-30 23:01:57 +0200

    Rust 2018 (stable): day 1 part 2

--- 2018/README.md
+++ 2018/README.md
@@ -3,6 +3,7 @@
 # Languages
 | rs |
 | -- |
+| [01.rs](/2018/rust/01.rs) | 
 | [06.rs](/2018/rust/06.rs) | 
 | [16.rs](/2018/rust/16.rs) | 
 | [17.rs](/2018/rust/17.rs) | 
--- 2018/rust/01.rs
+++ 2018/rust/01.rs
@@ -1,8 +1,22 @@
 // Advent of Code 2018, day 1
 // (c) aichingert
 
+use std::collections::HashSet;
+
 fn part1(frq: &Vec<i32>) -> i32 {
     frq.iter().sum::<i32>()
+}
+
+fn part2(frq: &Vec<i32>) -> i32 {
+    let (mut cur, mut pt) = (frq[0],0usize);
+    let mut frqs = HashSet::<i32>::new();
+
+    while frqs.insert(cur) {
+        if pt + 1 >= frq.len() { pt = 0; } else { pt += 1; }
+        cur += frq[pt];
+    }
+
+    cur
 }
 
 fn main() {
@@ -12,4 +26,5 @@ fn main() {
         .collect::<Vec<i32>>();
 
     println!("Part 1: {}", part1(&inp));
+    println!("Part 2: {}", part2(&inp));
 }
