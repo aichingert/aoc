01de58ff39970ed5a0a543ada43673e0ed45275c
aichingert <aichinger.tobias13@gmail.com>
2023-05-02 08:42:34 +0200

    Rust 2018 (stable): day 8 part 2

--- 2018/rust/08.rs
+++ 2018/rust/08.rs
@@ -3,14 +3,14 @@
 
 fn part1(tree: &[usize], cur: &mut usize) -> usize {
     let mut metadata = 0;
-    let mut am = tree[*cur + 1];
+    let amount_of_metadata = tree[*cur + 1];
 
     for _ in 0..tree[*cur] {
         *cur += 2;
         metadata += part1(tree, cur);
     }
 
-    for j in 0..am {
+    for _ in 0..amount_of_metadata {
         metadata += tree[*cur + 2];
         *cur += 1;
     } 
@@ -18,8 +18,28 @@ fn part1(tree: &[usize], cur: &mut usize) -> usize {
     metadata
 }
 
-fn part2(tree: &[usize]) -> i32 {
-    0
+fn part2(tree: &[usize], cur: &mut usize) -> usize {
+    let mut metadatas = Vec::<usize>::new();
+    let mut metadata = 0usize;
+    let amount_of_metadata = tree[*cur + 1];
+
+    for _ in 0..tree[*cur] {
+        *cur += 2;
+        metadatas.push(part2(tree, cur));
+    }
+
+    for _ in 0..amount_of_metadata {
+        metadata += if metadatas.len() > 0 {
+            if tree[*cur + 2] - 1 < metadatas.len() {
+                metadatas[tree[*cur + 2] - 1]
+            } else { 0 }
+        } else {
+            tree[*cur + 2]
+        };
+        *cur += 1;
+    } 
+
+    metadata
 }
 
 fn main() {
@@ -30,5 +50,5 @@ fn main() {
         .collect::<Vec<usize>>();
 
     println!("Part 1: {}", part1(&inp, &mut 0));
-    println!("Part 2: {}", part2(&inp));
+    println!("Part 2: {}", part2(&inp, &mut 0));
 }
--- README.md
+++ README.md
@@ -4,6 +4,7 @@
 | - |
 | [2015](/2015) |
 | [2016](/2016) |
+| [2017](/2017) |
 
 ## <a href="https://adventofcode.com/events"> To the problems </a>
 
