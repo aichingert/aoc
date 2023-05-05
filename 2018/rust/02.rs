43bf73d47ff5b97af5b1ea56ea2da6dadd9d1fa5
aichingert <aichinger.tobias13@gmail.com>
2023-05-05 07:38:03 +0200

    Rust 2018 (stable): day 2

--- 2018/rust/02.rs
+++ 2018/rust/02.rs
@@ -12,25 +12,43 @@ fn part1(lines: &Vec<Vec<char>>) -> i32 {
         for j in 0..lines[i].len() {
             *counter.entry(lines[i][j]).or_insert(0) += 1;
         }
-        let mut pw = false;
-        let mut ph = false;
 
-        for val in counter.values() {
-            if *val == 2 {
-                pw = true;
-            }
-
-            if *val == 3 {
-                ph = true;
-            }
-        }
-
-        if pw { two += 1; }
-        if ph { three += 1; }
-
+        if counter.values().any(|&x| x == 2) { two += 1; }
+        if counter.values().any(|&x| x == 3) { three += 1; }
     }
 
     two * three
+}
+
+fn part2(lines: &Vec<Vec<char>>) -> String {
+    for i in 0..lines.len() {
+        let mut diff;
+
+        for j in i+1..lines.len() {
+            if lines[j].len() != lines[i].len() { continue; }
+            diff = 0;
+
+            for k in 0..lines[i].len() {
+                if lines[i][k] != lines[j][k] {
+                    diff += 1;
+                }
+            }
+
+            if diff == 1 {
+                let mut res = Vec::<char>::new();
+
+                for k in 0..lines[i].len() {
+                    if lines[i][k] == lines[j][k] {
+                        res.push(lines[i][k]);
+                    }
+                }
+
+                return res.iter().collect::<String>();
+            }
+        }
+    }
+
+    "".to_string()
 }
 
 fn main() {
@@ -40,4 +58,5 @@ fn main() {
         .collect::<Vec<Vec<char>>>();
 
     println!("Part 1: {}", part1(&inp));
+    println!("Part 2: {}", part2(&inp));
 }
