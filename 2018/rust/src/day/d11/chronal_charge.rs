1321ba176dc47640dd88f6b7ba36b8506942f14b
aichingert <aichinger.tobias13@gmail.com>
2023-07-18 21:03:34 +0200

    feat: 2018 [rust] day 11 still slow but faster.

--- 2018/rust/src/day/d11/chronal_charge.rs
+++ 2018/rust/src/day/d11/chronal_charge.rs
@@ -2,69 +2,104 @@ use crate::day::{Input, InputError, InputResult, Output, Wrapper};
 
 const GRID_SIZE: usize = 300;
 
-fn solve(grid: &[[i32; GRID_SIZE]; GRID_SIZE], square: usize) -> ((usize, usize), i32) {
-    let (mut max, mut pos): (i32, (usize, usize)) = (0, (0, 0));
-
-    for i in 0..grid.len() - square {
-        for j in 0..grid[i].len() - square {
-            let sum = sum_square(grid, square, i, j);
-
-            if sum > max {
-                max = sum;
-                pos = (j, i);
-            }
-        }
-    }
-
-    (pos, max)
+#[derive(Clone)]
+struct Sizes {
+    squares: Vec<i32>,
 }
 
-fn sum_square(grid: &[[i32; GRID_SIZE]; GRID_SIZE], square: usize, i: usize, j: usize) -> i32 {
-    let mut sum: i32 = 0;
+impl Sizes {
+    fn new() -> Self {
+        Self {
+            squares: Vec::new(),
+        }
+    }
+}
 
-    for k in i..i + square {
-        for l in j..j + square {
-            sum += grid[k][l];
+fn get_from(x: usize, y: usize, serial_number: i32) -> i32 {
+    let rack_id: i32 = x as i32 + 10;
+    (((rack_id * y as i32) + serial_number) * rack_id / 100 % 10) - 5
+}
+
+fn sum_square_bet(
+    dp: &Vec<Vec<Sizes>>,
+    square: usize,
+    i: usize,
+    j: usize,
+    serial_number: i32,
+) -> i32 {
+    let mut sum: i32 = if square > 1 {
+        dp[i][j].squares[square - 2]
+    } else {
+        0
+    };
+
+    let y2 = i + square - 1;
+    let x1 = j + square - 1;
+    for k in 0..square {
+        let y1 = i + k;
+        let x2 = j + k;
+
+        if y1 == y2 && x1 == x2 {
+            sum += get_from(y1, x1, serial_number);
+        } else {
+            sum += get_from(y1, x1, serial_number);
+            sum += get_from(y2, x2, serial_number);
         }
     }
 
     sum
 }
 
-fn part_two(grid: &[[i32; GRID_SIZE]; GRID_SIZE]) -> (usize, usize, usize) {
-    let (mut max, mut pos, mut square): (i32, (usize, usize), usize) = (0, (0, 0), 0);
-    for i in 5..GRID_SIZE {
-        let (loc, sum) = solve(grid, i);
+fn pre_calculating(serial_number: i32) -> ((usize, usize), (usize, usize, usize)) {
+    let mut dp: Vec<Vec<Sizes>> = vec![vec![Sizes::new(); GRID_SIZE]; GRID_SIZE];
+    let mut pos: (usize, usize) = (0, 0);
+    let mut p2: (usize, usize, usize) = (0, 0, 0);
+    let mut m1 = 0;
+    let mut a = 0;
 
-        if max < sum {
-            max = sum;
-            pos = loc;
-            square = i;
+    // 1 2 3
+    // 4 5 6
+    // 7 8 9
+
+    // 1 2
+    // 4 5
+
+    // 2 3
+    // 5 6
+
+    for square in 1..14 {
+        for i in 0..GRID_SIZE - square {
+            for j in 0..GRID_SIZE - square {
+                let value = sum_square_bet(&dp, square, i, j, serial_number);
+                dp[i][j].squares.push(value);
+
+                if value > a {
+                    a = value;
+                    p2 = (i, j, square);
+                }
+            }
         }
     }
 
-    (pos.0, pos.1, square)
-}
-
-fn create_grid(serial_number: i32) -> [[i32; GRID_SIZE]; GRID_SIZE] {
-    let mut grid: [[i32; GRID_SIZE]; GRID_SIZE] = [[0; GRID_SIZE]; GRID_SIZE];
-
-    for y in 0..GRID_SIZE {
-        for x in 0..GRID_SIZE {
-            let rack_id: i32 = (x + 10) as i32;
-            grid[y][x] = (((rack_id * y as i32) + serial_number) * rack_id / 100 % 10) - 5;
+    for i in 0..GRID_SIZE - 3 {
+        for j in 0..GRID_SIZE - 3 {
+            if dp[i][j].squares[2] > m1 {
+                m1 = dp[i][j].squares[2];
+                pos = (i, j);
+            }
         }
     }
 
-    grid
+    (pos, p2)
 }
 
 pub fn run(input: Input) -> (Output, Output) {
     let input: i32 = input.unwrap();
-    let grid = create_grid(input);
 
-    let part_one: (usize, usize) = solve(&grid, 3).0;
-    let part_two: (usize, usize, usize) = part_two(&grid);
+    let pos = pre_calculating(input);
+    println!("{:?}", pos);
+    let part_one = pos.0;
+    let part_two = pos.1;
 
     (
         Output::S(format!("{},{}", part_one.0, part_one.1)),
