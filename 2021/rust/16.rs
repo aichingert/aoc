8a42d569096c7c9b66f986764aab85a42ce10af5
aichingert <aichinger.tobias13@gmail.com>
2023-09-01 14:39:42 +0200

    chore: improved 2021 [rust] day 16

--- 2021/rust/16.rs
+++ 2021/rust/16.rs
@@ -1,91 +1,59 @@
-#[derive(Clone)]
 struct Operator {
     type_id: u16,
     packets: Vec<Packet>
 }
 
-#[derive(Clone)]
-struct Literal {
-    value: u64,
-}
-
-#[derive(Clone)]
 enum Packet {
     Operator(Operator),
-    Literal(Literal),
+    Literal(u64),
 }
 
 fn main() {
-    let inp = std::fs::read_to_string("../input/16").unwrap();
-    let stream = to_bin(inp);
-
+    let stream = to_bin(std::fs::read_to_string("../input/16").unwrap());
     let mut part_one = 0;
+
     let packet = decode_package(&stream, &mut 0, &mut part_one);
 
     println!("Part 1: {}", part_one);
-    println!("Part 2: {}", evaluate(packet));
+    println!("Part 2: {}", evaluate(&packet));
 }
 
-fn evaluate(packet: Packet) -> u64 {
+fn evaluate(packet: &Packet) -> u64 {
     match packet {
-        Packet::Literal(literal) => {
-            literal.value
-        },
+        Packet::Literal(n) => *n,
         Packet::Operator(operator) => {
+            let pckts = &operator.packets;
             match operator.type_id {
-                0 => operator.packets.into_iter().map(|p| evaluate(p)).sum::<u64>(),
-                1 => operator.packets.into_iter().fold(1, |acc, p| acc * evaluate(p)),
-                2 => operator.packets.into_iter().fold(u64::MAX, |acc, p| acc.min(evaluate(p))),
-                3 => operator.packets.into_iter().fold(u64::MIN, |acc, p| acc.max(evaluate(p))),
-                5 => if evaluate(operator.packets[0].clone()) > evaluate(operator.packets[1].clone()) { 
-                    1 
-                } else { 
-                    0 
-                }
-                6 => if evaluate(operator.packets[0].clone()) < evaluate(operator.packets[1].clone()) { 
-                    1 
-                } else { 
-                    0 
-                }
-                7 => if evaluate(operator.packets[0].clone()) == evaluate(operator.packets[1].clone()) { 
-                    1 
-                } else { 
-                    0 
-                }
+                0 => pckts.iter().map(|p| evaluate(p)).sum::<u64>(),
+                1 => pckts.iter().fold(1, |acc, p| acc * evaluate(p)),
+                2 => pckts.iter().fold(u64::MAX, |acc, p| acc.min(evaluate(p))),
+                3 => pckts.iter().fold(u64::MIN, |acc, p| acc.max(evaluate(p))),
+                5 => if evaluate(&pckts[0]) > evaluate(&pckts[1]) { 1 } else { 0 }
+                6 => if evaluate(&pckts[0]) < evaluate(&pckts[1]) { 1 } else { 0 }
+                7 => if evaluate(&pckts[0]) == evaluate(&pckts[1]) { 1 } else { 0 }
                 _ => panic!("invalid operator id {}", operator.type_id),
             }
         },
     }
 }
 
-fn decode_package(stream: &Vec<u16>, i: &mut usize, versions: &mut u32) -> Packet {
-    let ptr = i;
-
-    let version = stream[*ptr..*ptr + 3].iter().rev().enumerate()
-        .map(|(p, bin)| bin * 2_u16.pow(p as u32))
-        .sum::<u16>();
+fn decode_package(stream: &Vec<u16>, ptr: &mut usize, versions: &mut u32) -> Packet {
+    let version = to_dec(&stream[*ptr..*ptr + 3]) as u32;
     *versions += version as u32;
-    let id = stream[*ptr + 3..*ptr + 6].iter().rev().enumerate()
-        .map(|(p, bin)| bin * 2_u16.pow(p as u32))
-        .sum::<u16>();
+
+    let id = to_dec(&stream[*ptr + 3..*ptr + 6]) as u16;
     *ptr += 6;
 
     match id {
         4 => {
-            let mut bits = Vec::<u16>::new();
+            let bits: usize = *ptr;
 
             while stream[*ptr] != 0 {
-                bits.extend_from_slice(&stream[*ptr+1..*ptr+5]);
                 *ptr += 5;
             }
-            bits.extend_from_slice(&stream[*ptr+1..*ptr+5]);
             *ptr += 5;
 
-            Packet::Literal(Literal {
-                value: bits.iter().rev().enumerate()
-                    .map(|(p, bin)| (*bin as u64) * 2_u64.pow(p as u32))
-                    .sum::<u64>()
-            })
+            Packet::Literal(to_dec(&stream[bits..*ptr]))
         },
         _ => {
             let mut operator = Operator {
@@ -95,9 +63,7 @@ fn decode_package(stream: &Vec<u16>, i: &mut usize, versions: &mut u32) -> Packe
 
             match stream[*ptr] {
                 0 => {
-                    let len = stream[*ptr + 1..*ptr + 16]
-                        .iter().rev().enumerate()
-                        .map(|(p, bin)| (*bin as usize) * 2_usize.pow(p as u32)).sum::<usize>();
+                    let len = to_dec(&stream[*ptr + 1..*ptr + 16]) as usize;
                     *ptr += 16;
                     let cur = *ptr;
 
@@ -106,9 +72,7 @@ fn decode_package(stream: &Vec<u16>, i: &mut usize, versions: &mut u32) -> Packe
                     }
                 },
                 1 => {
-                    let n = stream[*ptr + 1..*ptr + 12]
-                        .iter().rev().enumerate()
-                        .map(|(p, bin)| (*bin as usize) * 2_usize.pow(p as u32)).sum::<usize>();
+                    let n = to_dec(&stream[*ptr + 1..*ptr + 12]);
                     *ptr += 12;
 
                     for _ in 0..n {
@@ -121,6 +85,15 @@ fn decode_package(stream: &Vec<u16>, i: &mut usize, versions: &mut u32) -> Packe
             Packet::Operator(operator)
         }
     }
+}
+
+fn to_dec(s: &[u16]) -> u64 {
+    let len = s.len();
+
+    s.iter()
+        .enumerate()
+        .map(|(p, bin)| (*bin as u64) * 2_u64.pow((len as u32) - (p as u32) - 1))
+        .sum::<u64>()
 }
 
 fn to_bin(s: String) -> Vec<u16> {
@@ -144,7 +117,7 @@ fn to_bin(s: String) -> Vec<u16> {
             'D' => binary.extend_from_slice(&[1,1,0,1]),
             'E' => binary.extend_from_slice(&[1,1,1,0]),
             'F' => binary.extend_from_slice(&[1,1,1,1]),
-            _ => panic!("invalid hex char {c}"),
+            c => panic!("invalid hex char {}", c),
         };
     }
 
