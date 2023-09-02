1e59d6214e75385306ca79268898367533ee5dc6
aichingert <aichinger.tobias13@gmail.com>
2023-09-02 21:14:41 +0200

    feat: 2015 [haskell] day 2

--- 2015/haskell/02.hs
+++ 2015/haskell/02.hs
@@ -18,10 +18,18 @@ lineFromString s = sort (read l :  read w : read h : [])
 getWrappingPaper :: [Int] -> Int
 getWrappingPaper (l:w:h:[]) = 2 * l * w + 2 * w * h + 2 * h * l + l * w
 
+getRibbon :: [Int] -> Int
+getRibbon (l:w:h:[]) = 2*l + 2*w + l*w*h
+
 partOne :: [String] -> Int
 partOne inp = sum $ map getWrappingPaper $ map lineFromString inp
 
+partTwo :: [String] -> Int
+partTwo inp = sum $ map getRibbon $ map lineFromString inp
+
 main = do
   input <- readFile "../input/02"
-  putStrLn (show (partOne (lines input)))
+  let dimensions = lines input
+  putStrLn $ show $ partOne dimensions
+  putStrLn $ show $ partTwo dimensions
 
