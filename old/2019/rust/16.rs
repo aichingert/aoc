816afddfa0a06eb131b354c199eb9a959ab68751
aichingert <aichinger.tobias13@gmail.com>
2023-09-12 07:40:34 +0200

    feat: 2019 [rust] day 16 optimized part one, still not enough for part 2

--- old/2019/rust/16.rs
+++ old/2019/rust/16.rs
@@ -1,22 +1,25 @@
-const PATTERN: [i16;4] = [0, 1, 0, -1];
+const PATTERN: [i16;2] = [1, -1];
 
 fn phase(inp: Vec<u16>) -> Vec<u16> {
     let mut output = Vec::new();
 
     for i in 0..inp.len() {
-        let i = i;
-        let mut mathi = 0;
-        let mut ptr = 1;
+        let mut ptr = 0;
         let mut out = 0;
 
-        for j in i..inp.len() {
-            out += (inp[j] as i16) * PATTERN[ptr];
+        let mut j = i;
 
-            mathi += 1;
-            if mathi > i {
-                ptr = (ptr + 1) % PATTERN.len();
-                mathi = 0;
+        while j < inp.len() {
+            let mut s = 0;
+            while s <= i && j < inp.len() {
+                out += (inp[j] as i16) * PATTERN[ptr];
+
+                s += 1;
+                j += 1;
             }
+
+            j += i + 1;
+            ptr = 1 - ptr;
         }
 
         output.push((out.abs() as u16) % 10);
@@ -29,9 +32,9 @@ fn main() {
     let inp = std::fs::read_to_string("../input/16").unwrap().trim().to_string();
     let mut output = inp.chars().map(|c| (c as u8 - b'0').into()).collect::<Vec<u16>>();
 
-    for _ in 0..4 {
+    for _ in 0..100 {
         output = phase(output);
     }
 
-    println!("{:?}", &output);
+    println!("{:?}", &output[0..8]);
 }
