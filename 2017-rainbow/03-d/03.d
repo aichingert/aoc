d94f5686c16f0852cd64054ab5d52ef7a5f94e93
aichingert <aichinger.tobias13@gmail.com>
2023-09-21 08:51:13 +0200

    2017 [03 - d]

--- 2017-rainbow/03-d/03.d
+++ 2017-rainbow/03-d/03.d
@@ -4,41 +4,86 @@ import std.string;
 import std.conv;
 import std.math;
 
-int[2] ulam_spirale(int n) {
+class Point 
+{
+	int x, y;
+
+	this(int x, int y)
+	{
+		this.x = x;
+		this.y = y;
+	}
+
+	override size_t toHash() { return x * 1000 + y; }
+	override bool opEquals(Object o) 
+	{ 
+		Point p = cast(Point) o;
+		return p && x == p.x && y == p.y;
+	}
+}
+
+
+Point ulam_spirale(int n) 
+{
 	int k = to!int(ceil((sqrt(to!float(n)) - 1) / 2));
 	int t = 2*k+1;
 	int m = t*t;
 	t -= 1;
 
 	if (n >= m-t) {
-		return [k - (m-n), -k];
+		return new Point(k - (m-n), -k);
 	} else {
 		m -= t;
 	}
 
 	if (n >= m - t) {
-		return [-k,-k+(m-n)];
+		return new Point(-k,-k+(m-n));
 	} else {
 		m -= t;
 	}
 
 	if (n >= m - t) {
-		return [-k+(m-n),k];
+		return new Point(-k+(m-n),k);
 	} else {
-		return [k,k-(m-n-t)];
+		return new Point(k,k-(m-n-t));
 	}
 }
 
-int part_one(int n) {
-	int[2] cords = ulam_spirale(n);
-	return abs(cords[0]) + abs(cords[1]);
+int part_one(int n) 
+{
+	Point point = ulam_spirale(n);
+	return abs(point.x) + abs(point.y);
 }
 
-int part_two(int n) {
-   	return -1;
+int part_two(int n) 
+{
+	int[Point] spiral = [new Point(0,0): 1];
+	int[3] vec = [-1,0,1];
+	int ptr = 2;
+
+	for (;;)
+	{
+		int sum = 0;
+		int* cur;
+
+		Point p = ulam_spirale(ptr);
+
+		foreach (x; vec) {
+			foreach (y; vec) {
+				cur = new Point(p.x + x, p.y + y) in spiral;
+				if (cur !is null) { sum += *cur; } 
+			}
+		}
+
+		if (sum > n) { return sum; }
+
+		ptr += 1;
+		spiral[p] = sum;
+	}
 }
 
-void main() {
+void main() 
+{
    int inp = to!int(readText("../input/03").strip());
 
    writefln("Part one: %d", part_one(inp));
