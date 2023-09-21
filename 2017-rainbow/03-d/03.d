1f070eb1b6a0bdbf92da62cd1a21c4c5f2997c5b
aichingert <aichinger.tobias13@gmail.com>
2023-09-21 08:09:15 +0200

    2017 [03 - d] part 1

--- 2017-rainbow/03-d/03.d
+++ 2017-rainbow/03-d/03.d
@@ -2,17 +2,40 @@ import std.stdio;
 import std.file;
 import std.string;
 import std.conv;
+import std.math;
 
 int[2] ulam_spirale(int n) {
-   return [0, 0];
+	int k = to!int(ceil((sqrt(to!float(n)) - 1) / 2));
+	int t = 2*k+1;
+	int m = t*t;
+	t -= 1;
+
+	if (n >= m-t) {
+		return [k - (m-n), -k];
+	} else {
+		m -= t;
+	}
+
+	if (n >= m - t) {
+		return [-k,-k+(m-n)];
+	} else {
+		m -= t;
+	}
+
+	if (n >= m - t) {
+		return [-k+(m-n),k];
+	} else {
+		return [k,k-(m-n-t)];
+	}
 }
 
 int part_one(int n) {
-   return -1;
+	int[2] cords = ulam_spirale(n);
+	return abs(cords[0]) + abs(cords[1]);
 }
 
 int part_two(int n) {
-   return -1;
+   	return -1;
 }
 
 void main() {
