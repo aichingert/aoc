1a2ab2c8c5a914c555bcd8f3a62765c8db503e6b
aichingert <aichinger.tobias13@gmail.com>
2023-09-23 22:16:38 +0200

    feat: 2017 [04 - ada]

--- 2017-rainbow/04-ada/04.adb
+++ 2017-rainbow/04-ada/04.adb
@@ -1,7 +1,17 @@
 with Ada.Text_IO; use Ada.Text_IO;
+with Ada.Containers; use Ada.Containers;
 with Ada.Containers.Vectors;
 
 procedure Day_04 is
+	type Integer_Array is
+		array (1 .. 26) of Integer;
+
+	package Array_Vectors is new Ada.Containers.Vectors 
+		(Index_Type => Natural,
+		 Element_Type => Integer_Array);
+	use Array_Vectors;
+
+
 	F	   : File_Type;
 	File_Name  : constant String := "../input/04";
 
@@ -45,38 +55,63 @@ procedure Day_04 is
 
 	function Part_Two (Line : String) return Integer
 	is
-		This	   : Integer := 0;
-		Other	   : Integer := 0;
-		Last_Start : Natural := 0;
+		V 	: Vector;
+		Index	: Natural := 1;
+		Offset	: Natural := Character'Pos ('a') - 1;
 	begin
-		for I in Line'Range loop
-			if Line (I) = ' ' then
-				for J in I + 1 .. Line'Last loop
-					if Line (J) = ' ' then
-						if This = Other then
-							return 0;
+		loop
+			declare 
+				Counter	: Integer_Array := (others => 0);
+				Idx 	: Natural := 1;
+			begin
+				for I in Index .. Line'Last loop
+					if Line (I) = ' ' then
+						V.Append (Counter);
+						Index := I + 1;
+						exit;
+					else
+						Idx := Character'Pos (Line (I)) - Offset;
+						Counter (Idx) := Counter (Idx) + 1;
+
+						if I = Line'Last then
+							Index := Line'Last + 1;
+							V.Append (Counter);
+						end if;
+	 				end if;
+				end loop;
+			end;
+
+			exit when Index > Line'Last;
+		end loop;
+
+		for I in V.First_Index .. V.Last_Index - 1 loop
+			for J in I + 1 .. V.Last_Index loop
+				declare 
+					This	: constant Integer_Array := V (I);
+					Other 	: constant Integer_Array := V (J);
+					Index 	: Natural := 1;
+					Test	: Integer := 1;
+				begin
+					loop 
+						if This (Index) /= Other (Index) then
+							Test := Test + 1;
+							exit;
 						end if;
 
-						Other := 0;
-						Last_Start := J + 1;
-					else
-						Other := Other + Character'Pos (Line (J));
+						Index := Index + 1;
+						exit when Index > This'Last;
+					end loop;
+
+					if Index > This'Last then
+						return 0;
 					end if;
-				end loop;
-
-				if I + 1 /= Last_Start and then This = Other then
-					return 0;
-				end if;
-
-				This := 0;
-				Other := 0;
-			else
-				This := This + Character'Pos (Line (I));
-			end if;
+				end;
+			end loop;
 		end loop;
 
 		return 1;
 	end Part_Two;
+
 begin
 	Open (F, In_File, File_Name);
 
