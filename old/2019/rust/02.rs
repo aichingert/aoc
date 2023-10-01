10fcb4163e935e4bd11652ca14ec62c07431ba5a
aichingert <aichinger.tobias13@gmail.com>
2023-10-01 12:19:25 +0200

    feat: 2019 [rust] day 2 part 1 (reworking intcode)

--- old/2019/rust/02.rs
+++ old/2019/rust/02.rs
@@ -2,15 +2,23 @@
 // (c) aichingert
 
 #[path="intcode.rs"] mod intcode;
-use intcode::Computer;
+use intcode::{VM, Status};
 
 fn part1(opcodes: &Vec<i32>) -> i32 {
-    let mut computer: Computer = Computer::new(opcodes.clone(), vec![0]);
+    let mut vm: VM = VM::new(opcodes.clone());
 
-    computer.run();
-    computer.opcodes[0]
+    loop {
+        match vm.execute() {
+            Status::Exit => break,
+            _ => {},
+        };
+    }
+
+    vm.get_position(0)
 }
 
+
+/*
 fn part2(opcodes: &mut Vec<i32>) -> i32 {
     for noun in 0..100 {
         for verb in 0..100 {
@@ -29,12 +37,17 @@ fn part2(opcodes: &mut Vec<i32>) -> i32 {
 
     panic!("noun or verb is too small!");
 }
+*/
 
 fn main() {
-    let mut opcodes = std::fs::read_to_string("../input/02").unwrap().trim().split(',').map(|n| n.parse::<i32>().unwrap()).collect::<Vec<i32>>();
+    let mut opcodes = std::fs::read_to_string("../input/02")
+        .unwrap().trim()
+        .split(',')
+        .map(|n| n.parse::<i32>().unwrap())
+        .collect::<Vec<i32>>();
     opcodes[1] = 12;
-    opcodes[2] = 1;
+    opcodes[2] = 2;
 
     println!("Part 1: {}", part1(&opcodes));
-    println!("Part 2: {}", part2(&mut opcodes));
+    //println!("Part 2: {}", part2(&mut opcodes));
 }
--- old/2019/rust/intcode.rs
+++ old/2019/rust/intcode.rs
@@ -1,109 +1,53 @@
 // Advent of Code 2019, Intcode
 // (c) aichingert
 
-pub struct Computer {
-    pub opcodes: Vec<i32>,
-    pub input: Vec<i32>,
-    pub output: Vec<i32>,
+type N = i32;
+
+pub struct VM {
+    ptr: usize,
+    opcodes: Vec<N>,
 }
 
-impl Computer {
-    pub fn new(opcodes: Vec<i32>, input: Vec<i32>) -> Self {
-        Self { opcodes, input, output: Vec::<i32>::new() }
+pub enum Status {
+    Normal,
+    Exit,
+}
+
+impl VM {
+    pub fn new(opcodes: Vec<N>) -> Self {
+        Self {
+            ptr: 0,
+            opcodes,
+        }
     }
 
-    fn get_vals(&self, index: &mut usize, mode: &Vec<usize>) -> (usize,i32,i32) {
-        let mut locations = Vec::<usize>::new();
+    pub fn get_position(&self, dst: usize) -> N {
+        self.opcodes[dst]
+    }
 
-        for i in 2..4 {
-            match mode[3 - i] {
-                0 => locations.push(self.opcodes[*index+i-1] as usize),
-                1 => locations.push(*index+i-1),
-                _ => panic!("Inavlid mode: {}", mode[i]),
+    fn get_registers(&mut self) -> (usize, usize, usize) {
+        self.ptr += 4;
+        (self.opcodes[self.ptr - 1] as usize, self.opcodes[self.ptr - 3] as usize, self.opcodes[self.ptr - 2] as usize)
+    }
+
+    pub fn execute(&mut self) -> Status {
+        if self.ptr >= self.opcodes.len() || self.opcodes[self.ptr] == 99 {
+            return Status::Exit;
+        }
+
+        match self.opcodes[self.ptr] {
+            1 => {
+                let (dst, a, b) = self.get_registers();
+                self.opcodes[dst] = self.opcodes[a] + self.opcodes[b];
             }
-        }
-
-        *index += 4;
-        (self.opcodes[*index-1] as usize,self.opcodes[locations[0]],self.opcodes[locations[1]])
-    }
-
-    fn get_io(&self, index: &mut usize, mode: &Vec<usize>) -> usize {
-        *index += 2;
-        
-        match mode[1] {
-            0 => self.opcodes[*index-1] as usize,
-            1 => *index-1,
-            _ => panic!("Invalid mode: {}", mode[1]),
-        }
-    }
-
-    fn get_jump(&self, index: &mut usize, mode: &Vec<usize>) -> (usize,i32) {
-        let mut locations = Vec::<usize>::new();
-
-        for i in 2..4 {
-            match mode[3 - i] {
-                0 => locations.push(self.opcodes[*index+i-1] as usize),
-                1 => locations.push(*index+i-1),
-                _ => panic!("Invalid mode: {}", mode[i]),
+            2 => {
+                let (dst, a, b) = self.get_registers();
+                self.opcodes[dst] = self.opcodes[a] * self.opcodes[b];
             }
+            99 => return Status::Exit,
+            _  => panic!("invalid opcode {}", self.opcodes[self.ptr]),
         }
 
-        *index += 3;
-        (self.opcodes[locations[1]] as usize,self.opcodes[locations[0]])
-    }
-
-    pub fn run(&mut self) {
-        let mut index = 0usize;
-        let mut input = 0usize;
-
-        while index < self.opcodes.len() {
-            let mut commands = self.opcodes[index].to_string().chars().map(|ch| ch.to_digit(10).unwrap() as usize).collect::<Vec<usize>>();
-            while commands.len() < 4 { commands.insert(0, 0); }
-            let opcode = commands[2] * 10 + commands[3];
-                
-            match opcode {
-                1 => {
-                    let (at,a,b) = self.get_vals(&mut index, &commands);
-                    self.opcodes[at] = a + b;
-                },
-                2 => {
-                    let (at,a,b) = self.get_vals(&mut index, &commands);
-                    self.opcodes[at] = a * b;
-                },
-                3 => {
-                    let at = self.get_io(&mut index,&commands);
-                    self.opcodes[at] = self.input[input];
-                    input += 1;
-                },
-                4 => {
-                    let at = self.get_io(&mut index,&commands);
-                    self.output.push(self.opcodes[at]);
-                },
-                5 => {
-                    let (at,val) = self.get_jump(&mut index,&commands);
-                    if val != 0 { index = at; }
-                },
-                6 => {
-                    let (at,val) = self.get_jump(&mut index,&commands);
-                    if val == 0 { index = at; }
-                },
-                7 => {
-                    let (at,a,b) = self.get_vals(&mut index, &commands);
-                    match a < b { 
-                        true => self.opcodes[at] = 1,
-                        false => self.opcodes[at] = 0,
-                    };
-                },
-                8 => {
-                    let (at,a,b) = self.get_vals(&mut index, &commands);
-                    match a == b { 
-                        true => self.opcodes[at] = 1,
-                        false => self.opcodes[at] = 0,
-                    };
-                },
-                99 => break,
-                _ => panic!("Invalid opcode {}", opcode)
-            }
-        }
+        Status::Normal
     }
 }
