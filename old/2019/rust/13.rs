206d56d7a38b5edecb50eedb6ac5a945b3e3e5fc
aichingert <aichinger.tobias13@gmail.com>
2023-10-03 10:24:11 +0200

    feat: 2019 [rust] day 13

--- old/2019/rust/13.rs
+++ old/2019/rust/13.rs
@@ -1,6 +1,12 @@
 #[path="intcode.rs"] mod intcode;
 use intcode::{VM, Status, N, read_input};
 
+use std::collections::HashSet;
+use std::cmp::Ordering;
+
+const Y: usize = 19;
+const X: usize = 37;
+
 fn part_one(opcodes: &Vec<N>) -> N {
     let mut vm = VM::new(opcodes, 0);
     let mut status = Status::Normal;
@@ -13,13 +19,10 @@ fn part_one(opcodes: &Vec<N>) -> N {
             status = vm.exec();
 
             match status {
-                Status::Output(n) => match out.len() == 2 {
-                    true => {
-                        out.push(n);
-                        break;
-                    }
-                    false => out.push(n),
-                },
+                Status::Output(n) => {
+                    out.push(n);
+                    if out.len() > 2 { break; }
+                }
                 Status::Exit => break,
                 _ => {},
             }
@@ -35,36 +38,72 @@ fn part_one(opcodes: &Vec<N>) -> N {
 
 fn part_two(opcodes: &mut Vec<N>) -> N {
     opcodes[0] = 2;
-    let mut vm = VM::new(opcodes, -1);
 
-    let mut status = Status::Normal;
-    let mut tiles = 0;
-    
-    while status != Status::Exit {
+    let mut vm = VM::new(opcodes, 0);
+    let mut blocks: HashSet<(N, N)> = HashSet::new();
+    let mut paddle: (N, N) = (0, 0);
+    let mut ball: (N, N) = (0, 0);
+
+    for _ in 0..(Y + 1) * (X + 1) { 
         let mut out = Vec::<N>::new();
 
         loop {
-            status = vm.exec();
-
-            match status {
-                Status::Output(n) => match out.len() == 2 {
-                    true => {
-                        out.push(n);
-                        break;
-                    }
-                    false => out.push(n),
-                },
-                Status::Exit => break,
+            match vm.exec() {
+                Status::Output(n) => {
+                    out.push(n);
+                    if out.len() > 2 { break; }
+                }
                 _ => {},
             }
         }
 
-        if out.len() > 1 && out[2] == 2  {
-            tiles += 1;
-        }
+        match out[2] {
+            2 => { blocks.insert((out[1], out[0])); }
+            3 => paddle = (out[1], out[0]),
+            4 => ball = (out[1], out[0]),
+            _ => (),
+        };
     }
 
-    tiles
+    'outer: while blocks.len() > 0 {
+        let mut out = Vec::<N>::new();
+
+        loop {
+            match vm.exec() {
+                Status::Output(n) => { 
+                    out.push(n);
+                    if out.len() > 2 { break; }
+                }
+                Status::Exit => break 'outer,
+                _ => (),
+            }
+        }
+
+        let pt = (out[1], out[0]);
+
+        match out[2] {
+            0 => if blocks.contains(&pt) { blocks.remove(&pt); },
+            3 => paddle = pt,
+            4 => ball = pt,
+            _ => (),
+        };
+
+        vm._set_input(match ball.1.cmp(&paddle.1) {
+            Ordering::Less => -1,
+            Ordering::Equal => 0,
+            Ordering::Greater => 1,
+        });
+    }
+
+    let mut score = 0;
+    while score <= 0 {
+        match vm.exec() {
+            Status::Output(n) => score = n,
+            _ => (),
+        };
+    }
+    
+    score
 }
 
 fn main() {
