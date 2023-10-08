f3e0af26e85cd2d0a5effa7a209d70fe42422a46
aichingert <aichinger.tobias13@gmail.com>
2023-10-08 20:22:07 +0200

    feat: 2019 [rust] day 19

--- old/2019/rust/19.rs
+++ old/2019/rust/19.rs
@@ -1,5 +1,6 @@
 #[path="intcode.rs"] mod intcode;
 use intcode::{VM, Status, N, read_input};
+use std::collections::HashSet;
 
 fn part_one(opcodes: &Vec<N>) -> N {
     let mut ans = 0;
@@ -30,8 +31,53 @@ fn part_one(opcodes: &Vec<N>) -> N {
     ans
 }
 
+fn part_two(opcodes: &Vec<N>) -> N {
+    let mut beam = HashSet::new();
+
+    for i in 800..1200 {
+        for j in 800..1200 {
+            let mut vm = VM::new(opcodes, i);
+            let mut fs = true;
+            
+            loop {
+                if vm._get_next_opcode() == 3 {
+                    if fs {
+                        fs = false;
+                    } else {
+                        vm._set_input(j);
+                    }
+                }
+
+                match vm.exec() {
+                    Status::Output(n) => if n == 1 { beam.insert((i, j)); },
+                    Status::Exit => break,
+                    _ => {},
+                };
+            }
+        }
+    }
+
+    for i in 800..1100 {
+        'outer: for j in 800..1100 {
+            for r in i..i+100 {
+                for c in j..j+100 {
+                    if !beam.contains(&(r, c)) {
+                        continue 'outer;
+                    }
+                }
+            }
+
+            return i * 10000 + j;
+        }
+    }
+
+    panic!("no solution found");
+}
+
 fn main() {
     let opcodes = read_input(19);
 
     println!("Part one: {}", part_one(&opcodes));
+    println!("Part two: {}", part_two(&opcodes));
 }
+
