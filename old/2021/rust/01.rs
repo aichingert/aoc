35ca345c1bff1ec6861fc5e7c0542d3a64f1b617
aichingert <aichinger.tobias13@gmail.com>
2023-10-29 01:42:01 +0200

    feat: 2021 [rust] day 3

--- old/2021/rust/01.rs
+++ old/2021/rust/01.rs
@@ -1,10 +1,7 @@
-fn main() {
-    let inp = std::fs::read_to_string("../input/01").unwrap().trim().to_string();
-    let inp = inp.lines().map(|n| n.parse::<i32>().unwrap()).collect::<Vec<_>>();
-
+fn solve(inp: Vec<i32>) -> (u32, u32) {
     let mut part_one: u32 = 0;
     let mut part_two: u32 = 0;
-    
+
     for i in 0..inp.len() - 3 {
         if inp[i+1] > inp[i] {
             part_one += 1;
@@ -23,6 +20,15 @@ fn main() {
         part_one += 1;
     }
 
+    (part_one, part_two)
+}
+
+fn main() {
+    let inp = std::fs::read_to_string("../input/01").unwrap().trim().to_string();
+    let inp = inp.lines().map(|n| n.parse::<i32>().unwrap()).collect::<Vec<_>>();
+
+    let (part_one, part_two) = solve(inp);
+   
     println!("Part one: {}", part_one);
     println!("Part two: {}", part_two);
 }
