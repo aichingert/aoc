0cecc695aa40725d0284c34919e3f33ed5964d79
aichingert <aichinger.tobias13@gmail.com>
2023-11-24 20:25:18 +0100

    feat: improved 2020 rust day 21

--- old/2020/rs/21.rs
+++ old/2020/rs/21.rs
@@ -1,96 +1,85 @@
 use std::collections::{HashSet, HashMap};
 
-fn main() { 
-    let inp = std::fs::read_to_string("../input/21").unwrap().trim().to_string();
+fn part_one(allergen: &HashMap<String, Vec<String>>, ingredients: &Vec<Vec<String>>) -> u32 {
+    let mut bad = HashSet::new();
+    allergen.values().for_each(|vec| vec.iter().for_each(|s| { bad.insert(s); }));
 
-    let mut allergen: HashMap<String, HashSet<String>> = HashMap::new();
-    let mut ingredients: Vec<Vec<&str>> = Vec::new();
+    ingredients
+        .iter()
+        .map(|line| {
+            line.iter().filter(|s| !bad.contains(s)).count() as u32
+        })
+        .sum::<u32>()
+}
+
+fn part_two(mut allergen: HashMap<String, Vec<String>>, ingredients: &Vec<Vec<String>>) -> String {
+    let mut allergen_in_ingredient: Vec<(String, String)> = Vec::new();
+
+    while allergen.len() > 0 {
+        let keys = allergen.keys().filter(|&k| allergen[k].len() == 1).map(|s| s.to_string()).collect::<Vec<_>>();
+
+        for i in 0..keys.len() {
+            let current = allergen.remove(&keys[i]).unwrap();
+
+            for value in allergen.values_mut() {
+                if let Some(p) = value.iter().position(|x| x == &current[0]) {
+                    value.remove(p);
+                }
+            }
+
+            allergen_in_ingredient.push((keys[i].to_string(), current[0].clone()));
+        }
+    }
+
+    allergen_in_ingredient.sort_by(|a,b| a.0.cmp(&b.0));
+    let mut s = String::new();
+
+    for ingredient in allergen_in_ingredient {
+        s.push_str(&format!("{},", ingredient.1));
+    }
+    s.pop();
+
+    s
+}
+
+fn parse() -> (HashMap<String, Vec<String>>, Vec<Vec<String>>) {
+     let inp = std::fs::read_to_string("../input/21").unwrap().trim().to_string();
+
+    let mut allergen: HashMap<String, Vec<String>> = HashMap::new();
+    let mut ingredients: Vec<Vec<String>> = Vec::new();
 
     for line in inp.lines() {
         let (ing, all) = line.split_once(" (contains ").unwrap();
         let (ing, all) = (
-            ing.split(' ').collect::<Vec<_>>(), 
+            ing.split(' ').map(|s| s.to_string()).collect::<Vec<_>>(), 
             all[..all.len() - 1].split(", ").collect::<Vec<_>>()
         );
 
         for i in 0..all.len() {
             if let Some(has_to_contain) = allergen.get_mut(all[i]) {
-                let list = has_to_contain.clone();
-                let list = list.iter().map(|s| s.as_str()).collect::<Vec<&str>>();
+                let mut j = 0;
 
-                for j in 0..list.len() {
-                    if !ing.contains(&list[j]) {
-                        has_to_contain.remove(list[j]);
+                while j < has_to_contain.len() {
+                    if !ing.contains(&has_to_contain[j]) {
+                        has_to_contain.remove(j);
+                    } else {
+                        j += 1;
                     }
                 }
             } else {
-                let mut has_to_contain = HashSet::new();
-
-                for j in 0..ing.len() {
-                    has_to_contain.insert(ing[j].to_string());
-                }
-
-                allergen.insert(all[i].to_string(), has_to_contain);
+                allergen.insert(all[i].to_string(), ing.clone());
             }
         }
 
         ingredients.push(ing);
     }
 
-    let mut bad = HashSet::new();
+    (allergen, ingredients)
+}
 
-    for val in allergen.values() {
-        for s in val.iter() {
-            bad.insert(s.clone());
-        }
-    }
+fn main() { 
+    let (mut allergen, ingredients) = parse();
 
-    let mut ans = 0;
-
-    for i in 0..ingredients.len() {
-        for j in 0..ingredients[i].len() {
-            if !bad.contains(ingredients[i][j]) {
-                ans += 1;
-            }
-        }
-    }
-
-    let mut mapped: Vec<(String, String)> = Vec::new();
-
-    while allergen.len() > 0 {
-        let keys = allergen.clone();
-        let keys = keys.keys().collect::<Vec<_>>();
-        let mut pos = Vec::new();
-
-        for i in 0..keys.len() {
-            if allergen.get(keys[i]).and_then(|pos| if pos.len() == 1 { Some(pos) } else { None }).is_some() {
-                pos.push(keys[i]);
-            }
-        }
-
-        for i in 0..pos.len() {
-            let val = allergen.clone();
-            let val = val.get(pos[i]).unwrap().iter().collect::<Vec<_>>();
-            mapped.push((pos[i].to_string(), val[0].to_string()));
-
-            for k in 0..keys.len() {
-                if let Some(ing) = allergen.get_mut(keys[k]) {
-                    ing.remove(val[0]);
-                }
-            }
-
-            allergen.remove(pos[i]);
-        }
-    }
-
-    mapped.sort_by(|a,b| a.0.cmp(&b.0));
-
-    println!("Part one: {ans}");
-    print!("Part two: ");
-
-    for i in 0..mapped.len() - 1 {
-        print!("{},", mapped[i].1);
-    }
-
-    println!("{}", mapped[mapped.len() - 1].1);
+    println!("Part one: {}", part_one(&allergen, &ingredients));
+    println!("Part two: {}", part_two(allergen, &ingredients));
 }
