861ef3bb811f6c0e3df45f409fa200319d74d4c9
aichingert <aichinger.tobias13@gmail.com>
2023-12-01 14:49:59 +0100

    feat: 2023 [rust] day better part 1

--- old/2023/rs/01.rs
+++ old/2023/rs/01.rs
@@ -1,27 +1,16 @@
 fn part_one(inp: &Vec<Vec<char>>) -> u32 {
-    let mut ans = 0;
-    let (mut first, mut last) = (10, 0);
-
-    for i in 0..inp.len() {
-        for j in 0..inp[i].len() {
-            if inp[i][j] >= '0' && inp[i][j] <= '9' {
-                if first > 9 {
-                    first = (inp[i][j] as u8 - b'0') as u32;
-                }
-                last = (inp[i][j] as u8 - b'0') as u32;
-            }
-        }
-
-        ans += first * 10 + last;
-        first = 10;
-    }
-
-    ans
+    inp.iter()
+        .map(|chs| {
+            (*chs.iter().find(|c| c.is_digit(10)).unwrap() as u8 - b'0') as u32 * 10
+                + (*chs.iter().rev().find(|c| c.is_digit(10)).unwrap() as u8 - b'0') as u32
+        })
+    .sum::<u32>()
 }
 
 fn main() {
     let inp = std::fs::read_to_string("../input/01").unwrap().trim().to_string();
-    let inp = inp.lines().map(|n| n.chars().collect::<Vec<_>>()).collect::<Vec<_>>();
+    let inp = inp.lines().map(|c| c.chars().collect::<Vec<_>>()).collect::<Vec<_>>();
 
     println!("Part one: {}", part_one(&inp));
+    //println!("Part two: {}", part_two(&inp));
 }
