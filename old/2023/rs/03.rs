cf824d51020ce6e9f3478696d204927294cf62f6
aichingert <aichinger.tobias13@gmail.com>
2023-12-03 20:53:21 +0100

    feat: 2023 [rust] day 3

--- old/2023/rs/03.rs
+++ old/2023/rs/03.rs
@@ -1,64 +1,70 @@
-fn main() {
+use std::collections::HashMap;
+
+fn part_one(hm: &HashMap<(usize, usize), Vec<u32>>) -> u32 {
+    hm.values().map(|vec| vec.iter().sum::<u32>()).sum::<u32>()
+}
+
+fn part_two(inp: &Vec<Vec<char>>, hm: &HashMap<(usize, usize), Vec<u32>>) -> u32 {
+    hm.keys().filter(|(i, j)| inp[*i][*j] == '*')
+        .map(|k| {
+            if let Some(l) = hm.get(k).and_then(|l: &Vec<u32>| if l.len() != 2 { None } else { Some(l) }) {
+                l[0] * l[1]
+            } else { 0 }
+        })
+        .sum::<u32>()
+}
+
+fn parse() -> (Vec<Vec<char>>, HashMap<(usize, usize), Vec<u32>>) {
     let inp = std::fs::read_to_string("../input/03").unwrap().trim().to_string();
     let inp = inp.lines().map(|l| l.chars().collect::<Vec<_>>()).collect::<Vec<_>>();
-
-    let mut ans = 0;
+    let mut hm = HashMap::new();
 
     for i in 0..inp.len() {
         for j in 0..inp[i].len() {
-            if !(inp[i][j] >= '0' && inp[i][j] <= '9') && inp[i][j] == '*' {
-                let mut seen = Vec::new();
-                let mut has = Vec::new();
-                for y in [(-1,0),(0,-1),(1,0),(0,1),(1,1),(1,-1),(-1,-1),(-1,1)] {
-                    let (nx, ny) = (j as i32 + y.0, i as i32 + y.1);
-
-                    if  nx < 0 || ny < 0 || nx >= inp[i].len() as i32 || ny >= inp.len() as i32 {
-                        continue;
-                    }
-
-                    let (ny, nx) = (ny as usize, nx as usize);
-
-                    if inp[ny][nx] >= '0' && inp[ny][nx] <= '9' {
-                        println!("{ny}:{nx} => {}", inp[ny][nx]);
-                        let mut chs = Vec::new();
-                        let mut x = 1 + nx;
-
-                        if seen.contains(&(ny, nx)) {
-                            continue;
-                        }
-
-                        while x < inp[ny].len() && inp[ny][x] >= '0' && inp[ny][x] <= '9' {
-                            seen.push((ny, x));
-                            chs.push(inp[ny][x]);
-                            x += 1;
-                        }
-
-                        x = nx;
-                        while x > 0 && inp[ny][x] >= '0' && inp[ny][x] <= '9' {
-                            seen.push((ny, x));
-                            chs.insert(0, inp[ny][x]);
-                            x -= 1;
-                        }
-
-                        if inp[ny][x] >= '0' && inp[ny][x] <= '9' {
-                            seen.push((ny, x));
-                            chs.insert(0, inp[ny][x]);
-                        }
-
-                        let s = chs.iter().collect::<String>();
-                        //ans += s.parse::<u32>().unwrap();
-                        has.push(s.parse::<u32>().unwrap());
-                    }
-                }
-
-                if has.len() == 2 {
-                    ans += has[0] * has[1];
-                }
-                println!("{:?}", seen);
+            if !inp[i][j].is_digit(10) && inp[i][j] != '.' {
+                hm.insert((i, j), extract_numbers_around(&inp, i, j));
             }
         }
     }
 
-    println!("{}", ans);
+    (inp, hm)
+}
 
+fn extract_numbers_around(inp: &Vec<Vec<char>>, i: usize, j: usize) -> Vec<u32> {
+    let mut seen = Vec::new();
+    let mut nums = Vec::new();
+
+    for (x,y) in [(-1,0),(0,-1),(1,0),(0,1),(1,1),(1,-1),(-1,-1),(-1,1)] {
+        let (nx, ny) = (j as i32 + x, i as i32 + y);
+
+        if seen.contains(&(ny, nx)) || (nx < 0 || ny < 0 || ny >= inp.len() as i32 || nx >= inp[i].len() as i32) {
+            continue;
+        }
+        let (nx, ny) = (nx as usize, ny as usize);
+
+        if inp[ny][nx].is_digit(10) {
+            let (mut lhs, mut rhs) = (nx as i32, nx);
+
+            while rhs + 1 < inp[i].len() && inp[ny][rhs + 1].is_digit(10) {
+                seen.push((ny as i32, rhs as i32 + 1));
+                rhs += 1;
+            }
+
+            while lhs - 1 >= 0 && inp[ny][lhs as usize - 1].is_digit(10) {
+                seen.push((ny as i32, lhs - 1));
+                lhs -= 1;
+            }
+
+            nums.push(inp[ny][lhs as usize..=rhs].iter().collect::<String>().parse::<u32>().unwrap());
+        }
+    }
+
+    nums
+}
+
+fn main() {
+    let (inp, hm) = parse();
+
+    println!("Part one: {}", part_one(&hm));
+    println!("Part two: {}", part_two(&inp, &hm));
 }
