3129bdb81ac741aae307056f98332669f0d9a65d
aichingert <aichinger.tobias13@gmail.com>
2023-12-10 10:25:38 +0100

    feat: 2023 (rust) day 10 better part 1

--- old/2023/rs/10.rs
+++ old/2023/rs/10.rs
@@ -1,225 +1,117 @@
-use std::collections::{HashMap, VecDeque, HashSet};
+use std::collections::{HashMap, VecDeque};
 
-fn main() {
-    let inp = std::fs::read_to_string("../input/10").unwrap().trim().to_string();
-    let inp = inp.lines().map(|l| l.chars().collect::<Vec<_>>()).collect::<Vec<_>>();
+type Point = (usize, usize);
 
+fn append_points(cur: (Point, u32), inp: &Vec<Vec<char>>, directions: Vec<(i32, i32)>, 
+    bfs: &mut VecDeque<(Point, u32)>, cond: impl Fn(char, (i32, i32)) -> bool) 
+{
+    let ((y, x), d) = cur;
+
+    for dir in directions {
+        let (ny, nx) = (y as i32 + dir.0, x as i32 + dir.1);
+
+        if ny < 0 || nx < 0 || ny >= inp.len() as i32 || nx >= inp[y].len() as i32 {
+            continue;
+        }
+
+        let (ny, nx) = (ny as usize, nx as usize);
+
+        if cond(inp[ny][nx], dir) {
+            bfs.push_back(((ny, nx), d + 1));
+        }
+    }
+}
+
+fn get_loop(inp: &Vec<Vec<char>>) -> HashMap<(usize, usize), u32> {
     let mut starting = (0, 0);
 
-    for i in 0..inp.len() {
+    'search: for i in 0..inp.len() {
         for j in 0..inp[i].len() {
             match inp[i][j] {
-                'S' => starting = (i, j),
+                'S' => {
+                    starting = (i, j);
+                    break 'search;
+                }
                 _ => (),
             }
         }
     }
 
-    let mut bfs = VecDeque::from([(starting, 0)]);
-    let mut dis = HashMap::new();
+    let pos = [vec![(1,0), (-1,0)], vec![(0,1),(0,-1)],vec![(-1,0),(0,-1)],vec![(-1,0),(0,1)],vec![(1,0),(0,-1)],vec![(1,0),(0,1)]];
 
-    while let Some(((y, x), d)) = bfs.pop_front() {
-        if let Some(id) = dis.get(&(y, x)) {
-            if *id > d {
-                dis.insert((y, x), d);
-            } else {
+    'lp: for tiles in pos {
+        let mut bfs: VecDeque<((usize, usize), u32)> = VecDeque::new();
+        let mut dist = HashMap::from([(starting, 0)]);
+
+        for tile in tiles {
+            let (y, x) = (starting.0 as i32 + tile.0, starting.1 as i32 + tile.1);
+
+            if y < 0 || x < 0 || y >= inp.len() as i32 || x >= inp[0].len() as i32 {
+                continue 'lp;
+            }
+
+            bfs.push_front(((y as usize, x as usize), 0));
+        }
+
+        while let Some(((y, x), d)) = bfs.pop_front() {
+            if dist.insert((y, x), d).is_some() {
                 continue;
             }
-        } else {
-            dis.insert((y, x), d);
+
+            match inp[y][x] {
+                '-' => append_points(((y, x), d), inp, vec![(0,1),(0,-1)], &mut bfs, |ch: char, vec: (i32, i32)| {
+                    match (ch, vec.1) {
+                        ('-', _) | ('J', 1) | ('7', 1) | ('L', -1) | ('F', -1) => true,
+                        _ => false,
+                    }
+                }),
+                '|' => append_points(((y, x), d), inp, vec![(1,0),(-1,0)], &mut bfs, |ch: char, vec: (i32, i32)| {
+                    match (ch, vec.0) {
+                        ('|', _) | ('J', 1) | ('7', -1) | ('L', 1) | ('F', -1) => true,
+                        _ => false,
+                    }
+                }),
+                'L' => append_points(((y, x), d), inp, vec![(0,1),(-1,0)], &mut bfs, |ch: char, vec: (i32, i32)| {
+                    match (ch, vec.1) {
+                        ('-', 1) | ('|', 0) | ('J', 1) | ('7', _) | ('F', 0) => true,
+                        _ => false,
+                    }
+                }),
+                'J' => append_points(((y, x), d), inp, vec![(0,-1),(-1,0)], &mut bfs, |ch: char, vec: (i32, i32)| {
+                    match (ch, vec.1) {
+                        ('-', -1) | ('|', 0) | ('L', -1) | ('7', 0) | ('F', _) => true,
+                        _ => false,
+                    }
+                }),
+                '7' => append_points(((y, x), d), inp, vec![(0,-1),(1,0)], &mut bfs, |ch: char, vec: (i32, i32)| {
+                    match (ch, vec.1) {
+                        ('-', -1) | ('|', 0) | ('L', _) | ('J', 0) | ('F', -1) => true,
+                        _ => false,
+                    }
+                }),
+                'F' => append_points(((y, x), d), inp, vec![(0,1),(1,0)], &mut bfs, |ch: char, vec: (i32, i32)| {
+                    match (ch, vec.1) {
+                        ('-', 1) | ('|', 0) | ('L', 0) | ('J', _) | ('7', 1) => true,
+                        _ => false,
+                    }
+                }),
+                _ => break,
+            }
         }
 
-        match inp[y][x] {
-            '-' => {
-                for vec in [(0,1),(0,-1)] {
-                    let (ny, nx) = (y as i32 + vec.0, x as i32 + vec.1);
-
-                    if ny < 0 || nx < 0 || ny >= inp.len() as i32 || nx >= inp[0].len() as i32 {
-                        continue;
-                    }
-
-                    let (ny, nx) = (ny as usize, nx as usize);
-
-                    match (inp[ny][nx], vec.1) {
-                        ('-', _) => { bfs.push_back(((ny, nx), d + 1)); }
-                        ('J', 1) => { bfs.push_back(((ny, nx), d + 1)); }
-                        ('7', 1) => { bfs.push_back(((ny, nx), d + 1)); }
-                        ('L', -1) => { bfs.push_back(((ny, nx), d + 1)); }
-                        ('F', -1) => { bfs.push_back(((ny, nx), d + 1)); }
-                        _ => (),
-                    }
-                }
-            }
-            '|' => {
-                for vec in [(1,0),(-1,0)] {
-                    let (ny, nx) = (y as i32 + vec.0, x as i32 + vec.1);
-
-                    if ny < 0 || nx < 0 || ny >= inp.len() as i32 || nx >= inp[0].len() as i32 {
-                        continue;
-                    }
-
-                    let (ny, nx) = (ny as usize, nx as usize);
-
-                    match (inp[ny][nx], vec.0) {
-                        ('|', _) => { bfs.push_back(((ny, nx), d + 1)); }
-                        ('L', 1) => { bfs.push_back(((ny, nx), d + 1)); }
-                        ('J', 1) => { bfs.push_back(((ny, nx), d + 1)); }
-                        ('F', -1) => { bfs.push_back(((ny, nx), d + 1)); }
-                        ('7', -1) => { bfs.push_back(((ny, nx), d + 1)); }
-                        _ => (),
-                    }
-                }
-            }
-            'L' => {
-                for vec in [(0,1),(-1,0)] {
-                    let (ny, nx) = (y as i32 + vec.0, x as i32 + vec.1);
-
-                    if ny < 0 || nx < 0 || ny >= inp.len() as i32 || nx >= inp[0].len() as i32 {
-                        continue;
-                    }
-
-                    let (ny, nx) = (ny as usize, nx as usize);
-
-                    match (inp[ny][nx], vec.1) {
-                        ('-', 1) => { bfs.push_back(((ny, nx), d + 1)); }
-                        ('J', 1) => { bfs.push_back(((ny, nx), d + 1)); }
-                        ('7', _) => { bfs.push_back(((ny, nx), d + 1)); }
-                        ('|', 0) => { bfs.push_back(((ny, nx), d + 1)); }
-                        ('F', 0) => { bfs.push_back(((ny, nx), d + 1)); }
-                        _ => (),
-                    }
-                }
-            }
-            'J' => {
-                for vec in [(0,-1),(-1,0)] {
-                    let (ny, nx) = (y as i32 + vec.0, x as i32 + vec.1);
-
-                    if ny < 0 || nx < 0 || ny >= inp.len() as i32 || nx >= inp[0].len() as i32 {
-                        continue;
-                    }
-
-                    let (ny, nx) = (ny as usize, nx as usize);
-
-                    match (inp[ny][nx], vec.1) {
-                        ('-', -1) => { bfs.push_back(((ny, nx), d + 1)); }
-                        ('L', -1) => { bfs.push_back(((ny, nx), d + 1)); }
-                        ('|', 0) => { bfs.push_back(((ny, nx), d + 1)); }
-                        ('F', _) => { bfs.push_back(((ny, nx), d + 1)); }
-                        ('7', 0) => { bfs.push_back(((ny, nx), d + 1)); }
-                        _ => (),
-                    }
-                }
-            }
-            '7' => {
-                for vec in [(0,-1),(1,0)] {
-                    let (ny, nx) = (y as i32 + vec.0, x as i32 + vec.1);
-
-                    if ny < 0 || nx < 0 || ny >= inp.len() as i32 || nx >= inp[0].len() as i32 {
-                        continue;
-                    }
-
-                    let (ny, nx) = (ny as usize, nx as usize);
-
-                    match (inp[ny][nx], vec.1) {
-                        ('-', -1) => { bfs.push_back(((ny, nx), d + 1)); }
-                        ('L', _) => { bfs.push_back(((ny, nx), d + 1)); }
-                        ('F', -1) => { bfs.push_back(((ny, nx), d + 1)); }
-                        ('|', 0) => { bfs.push_back(((ny, nx), d + 1)); }
-                        ('J', 0) => { bfs.push_back(((ny, nx), d + 1)); }
-                        _ => (),
-                    }
-                }
-            }
-            'F' => {
-                for vec in [(0,1),(1,0)] {
-                    let (ny, nx) = (y as i32 + vec.0, x as i32 + vec.1);
-
-                    if ny < 0 || nx < 0 || ny >= inp.len() as i32 || nx >= inp[0].len() as i32 {
-                        continue;
-                    }
-
-                    let (ny, nx) = (ny as usize, nx as usize);
-
-                    match (inp[ny][nx], vec.1) {
-                        ('-', 1) => { bfs.push_back(((ny, nx), d + 1)); }
-                        ('7', 1) => { bfs.push_back(((ny, nx), d + 1)); }
-                        ('J', _) => { bfs.push_back(((ny, nx), d + 1)); }
-                        ('|', 0) => { bfs.push_back(((ny, nx), d + 1)); }
-                        ('L', 0) => { bfs.push_back(((ny, nx), d + 1)); }
-                        _ => (),
-                    }
-                }
-            }
-            'S' => {
-                //bfs.push_back(((y, x + 1), 1));
-                //bfs.push_back(((y, x - 1), 1));
-                //real ^
-
-                bfs.push_back(((y + 1, x), 1));
-                bfs.push_back(((y, x + 1), 1));
-            }
-            x => println!("whu: {:?} {x}", inp),
+        if dist.values().filter(|&&x| x == 2).count() == 2 {
+            return dist;
         }
     }
 
-    let mut walls = HashSet::new();
+    panic!("No loop found");
+}
 
-    for ((y, x), _) in &dis {
-        if inp[*y][*x] == '|' || inp[*y][*x] == '-' {
-            walls.insert((*y, *x));
-        }
-    }
+fn main() {
+    let inp = std::fs::read_to_string("../input/10").unwrap().trim().to_string();
+    let inp = inp.lines().map(|l| l.chars().collect::<Vec<_>>()).collect::<Vec<_>>();
 
-    for i in 0..inp.len() {
-        for j in 0..inp[i].len() {
-            if let Some(_) = dis.get(&(i, j)) {
-                print!("#");
-            } else {
-                print!(".");
-            }
-        }
-        println!();
-    }
+    let dist = get_loop(&inp);
 
-    let mut ans = 0;
-
-    for i in 0..inp.len() {
-        for j in 0..inp[i].len() {
-            let mut bfs = VecDeque::from([(i, j)]);
-            let mut sen = HashSet::from([(i, j)]);
-            let mut out = false;
-
-            'lp: while let Some((y, x)) = bfs.pop_front() {
-                for vec in [(0,1),(1,0),(-1,0),(0,-1)] {
-                    let (ny, nx) = (y as i32 + vec.0, x as i32 + vec.1);
-
-                    if ny < 0 || nx < 0 || ny >= inp.len() as i32 || nx >= inp[i].len() as i32 {
-                        out = true;
-                        break 'lp;
-                    }
-
-                    let (ny, nx) = (ny as usize, nx as usize);
-                    if !sen.insert((ny, nx)) {
-                        continue;
-                    }
-
-                    if walls.contains(&(ny, nx)) {
-                        if inp[ny][nx] == '|' && vec.1 != 0 || inp[ny][nx] == '-' && vec.0 != 0 {
-                            bfs.push_back((ny, nx));
-                        }
-                    } else {
-                        bfs.push_back((ny, nx));
-                    }
-                }
-            }
-
-            if !out {
-                println!("{:?}", (i, j));
-                ans += 1;
-            }
-        }
-    }
-
-    println!("{:?}", dis.values().max());
-    println!("{:?}", ans);
+    println!("{}", dist.values().max().unwrap());
 }
