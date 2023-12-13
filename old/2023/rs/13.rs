305f74b6324e0809d95c4862bba660eb008e79c9
aichingert <aichinger.tobias13@gmail.com>
2023-12-13 20:28:54 +0100

    feat: aua...

--- old/2023/rs/13.rs
+++ old/2023/rs/13.rs
@@ -1,63 +1,73 @@
+fn which(chs: &Vec<Vec<char>>, ch: i32) -> Option<u32> {
+    for i in 1..chs.len() {
+        let mut rp = i;
+        let mut mp = i as i32 - 1;
+
+        while mp >= 0 && rp < chs.len() && chs[rp] == chs[mp as usize] {
+            rp += 1;
+            mp -= 1;
+        }
+
+        if mp < ch && ch < rp as i32 && (rp == chs.len() || mp == -1) {
+            return Some(i as u32);
+        }
+    }
+
+    None
+}
+
+fn get_reflection(map: &Vec<Vec<char>>, r: i32, c: i32) -> Option<u32> {
+    let mut rows = Vec::<Vec<char>>::new();
+    let mut cols = Vec::<Vec<char>>::new();
+
+    for i in 0..map.len() {
+        rows.push(map[i].clone());
+    }
+
+    if let Some(n) = which(&rows, r) {
+        return Some(n * 100);
+    }
+
+    for j in 0..map[0].len() {
+        let mut col = Vec::new();
+
+        for i in 0..map.len() {
+            col.push(map[i][j]);
+        }
+
+        cols.push(col);
+    }
+
+    which(&cols, c)
+}
+
 fn main() {
     let inp = std::fs::read_to_string("../input/13").unwrap().trim().to_string();
     let inp = inp.split("\n\n").collect::<Vec<_>>();
 
     let mut ans = 0;
+    let mut p2ans = 0;
 
-    for pattern in inp {
-        let map = pattern.lines().map(|l| l.chars().collect::<Vec<_>>()).collect::<Vec<_>>();
+    'lp: for pattern in inp {
+        let mut map = pattern.lines().map(|l| l.chars().collect::<Vec<_>>()).collect::<Vec<_>>();
+        //ans += get_reflection(&map, 0, 0).unwrap();
 
-        let mut split = (map[0].len() as f32 / 2.0).ceil() as usize;
-        let mut rows = 0;
-        let mut cols = 0;
+        for i in 0..map.len() {
+            for j in 0..map[i].len() {
+                map[i][j] = if map[i][j] == '#' { '.' } else { '#' };
 
-        for i in 0..split {
-            let mut row = Vec::new();
-            let mut mir = Vec::new();
-
-            let m_idx = split * 2 - i - 1;
-
-            if m_idx < map[0].len() {
-                for j in 0..map.len() {
-                    row.push(map[j][i]);
-                    mir.push(map[j][m_idx]);
+                if let Some(n) = get_reflection(&map, i as i32, j as i32) {
+                    p2ans += n;
+                    continue 'lp;
                 }
+
+                map[i][j] = if map[i][j] == '#' { '.' } else { '#' };
             }
-
-
-            if row != mir {
-                break;
-            }
-
-            rows += 1;
         }
 
-        split = (map.len() as f32 / 2.0).ceil() as usize;
-
-        for i in 0..split {
-            let mut col = Vec::new();
-            let mut mir = Vec::new();
-
-            let m_idx = split * 2 - i - 1;
-
-            if m_idx < map.len() {
-                for j in 0..map[0].len() {
-                    col.push(map[i][j]);
-                    mir.push(map[m_idx][j]);
-                }
-            }
-
-            if col != mir {
-                break;
-            }
-            cols += 1;
-        }
-
-        if cols >= rows {
-            ans += cols * 100;
-        } else {
-            ans += rows;
-        }
+        
     }
+
     println!("{ans}");
+    println!("{p2ans}");
 }
