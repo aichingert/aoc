593fa9c48abafb075f9d0c36950ef04f8b2a570a
aichingert <aichinger.tobias13@gmail.com>
2024-02-04 00:28:56 +0100

    feat: 2017 [fortran] moved shuffle into single function

--- 2017-rainbow/10-for/10.f90
+++ 2017-rainbow/10-for/10.f90
@@ -1,17 +1,13 @@
 program day10
 implicit none
 
-    integer :: io
-    integer :: idx = 1
-    integer :: pos = 1
-    integer :: skip_size = 0
+    integer :: io, skip_size = 0, idx = 1
 
-    integer :: over = 0
     integer :: len_size = 16
     integer :: buffer_size = 256
 
-    integer, dimension(16) :: lengths ! 16
-    integer, dimension(256) :: buffer  ! 256
+    integer, dimension(16) :: lengths
+    integer, dimension(256) :: buffer
 
     open (newunit=io, file='../input/10', status='old', action='read')
     read (io, *) lengths
@@ -21,30 +17,38 @@ implicit none
         buffer(idx) = idx - 1
     end do
 
-    idx = 1
-    do while (idx <= len_size)
-        call rev(pos, pos + lengths(idx) - 1, buffer, buffer_size)
-
-        pos = pos + lengths(idx) + skip_size
-        skip_size = skip_size + 1
-        idx = idx + 1
-
-        if (pos > buffer_size) then 
-            over = pos / buffer_size
-            pos = modulo(pos, buffer_size + 1) + over
-        end if
-    end do
-
+    call shuffle(skip_size, lengths, len_size, buffer, buffer_size)
+    
     print *, "Part one: ", buffer(1) * buffer(2)
 end program day10
+
+subroutine shuffle(skip_size, lengths, l_size, buffer, b_size)
+implicit none
+    
+integer, intent (in)  :: l_size, b_size
+integer, intent (in)  :: lengths(l_size)
+integer, intent (inout) :: skip_size, buffer(b_size)
+
+    integer :: i = 1, j = 1
+
+    do while (i <= l_size)
+        call rev(j, j + lengths(i) - 1, buffer, b_size)
+
+        j = j + lengths(i) + skip_size
+        skip_size = skip_size + 1
+        i = i + 1
+
+        if (j > b_size) j = modulo(j, b_size + 1) + j / b_size
+    end do
+end subroutine shuffle
 
 subroutine rev(src, dst, data, size)
 implicit none
 
 integer, intent (in)  :: src, dst, size
-integer, intent (out) :: data(size)
+integer, intent (inout) :: data(size)
 
-    integer :: lpsz, tmp, sp, dp, ov
+    integer :: lpsz, tmp, sp, dp, ov = 1
 
     lpsz = (dst - src) / 2
     sp = src
