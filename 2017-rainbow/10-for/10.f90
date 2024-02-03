49d34ce7bc989449d3e51da216e88836f9f28304
aichingert <aichinger.tobias13@gmail.com>
2024-02-03 17:04:37 +0100

    feat: 2017 [fortran] day 10 part 1

--- 2017-rainbow/10-for/10.f90
+++ 2017-rainbow/10-for/10.f90
@@ -6,27 +6,36 @@ implicit none
     integer :: pos = 1
     integer :: skip_size = 0
 
-    integer, dimension(4) :: lengths ! 16
-    integer, dimension(5) :: buffer  ! 256
+    integer :: over = 0
+    integer :: len_size = 16
+    integer :: buffer_size = 256
+
+    integer, dimension(16) :: lengths ! 16
+    integer, dimension(256) :: buffer  ! 256
 
     open (newunit=io, file='../input/10', status='old', action='read')
     read (io, *) lengths
     close(io)
 
-    do idx = 1,5
+    do idx = 1,buffer_size
         buffer(idx) = idx - 1
     end do
-    
 
     idx = 1
-    do while (idx < 5)
-        call rev(pos, lengths(idx), buffer, 5)
-        pos = modulo(pos + lengths(idx) + skip_size, 6)
+    do while (idx <= len_size)
+        call rev(pos, pos + lengths(idx) - 1, buffer, buffer_size)
+
+        pos = pos + lengths(idx) + skip_size
         skip_size = skip_size + 1
         idx = idx + 1
-        print *, buffer
+
+        if (pos > buffer_size) then 
+            over = pos / buffer_size
+            pos = modulo(pos, buffer_size + 1) + over
+        end if
     end do
 
+    print *, "Part one: ", buffer(1) * buffer(2)
 end program day10
 
 subroutine rev(src, dst, data, size)
@@ -35,19 +44,29 @@ implicit none
 integer, intent (in)  :: src, dst, size
 integer, intent (out) :: data(size)
 
-    integer :: tmp, sp, dp
+    integer :: lpsz, tmp, sp, dp, ov
 
+    lpsz = (dst - src) / 2
     sp = src
     dp = dst
 
-    do while (sp /= dp .and. sp + 1 /= dp)
+    do while (lpsz > 0)
+        if (dp > size) then
+            ov = dp / size
+            dp = modulo(dp, size + 1) + ov
+        else if (dp < 1) then
+            dp = size
+        end if
+        if (sp > size) then
+            sp = modulo(sp, size + 1) + 1
+        end if
+
         tmp = data(sp)
         data(sp) = data(dp)
         data(dp) = tmp
 
-        sp = modulo(sp + 1, size + 1)
+        sp = sp + 1
         dp = dp - 1
-
-        if (dp < 0) dp = size
+        lpsz = lpsz - 1
     end do
 end subroutine rev
