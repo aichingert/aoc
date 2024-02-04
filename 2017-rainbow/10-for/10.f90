86792a344e3c3ec88baa197772c5e7e92bea6fb5
aichingert <aichinger.tobias13@gmail.com>
2024-02-04 16:35:33 +0100

    feat: 2017 [fortran] day 10 done

--- 2017-rainbow/10-for/10.f90
+++ 2017-rainbow/10-for/10.f90
@@ -2,52 +2,17 @@
 ! most likely the same lenght for everyone and I did not figure out how
 ! to do this better. So if you run into any problems try to change the numbers
 
-function part_two() result(ans)
-implicit none
-    
-    integer :: io, idx = 1, pos = 1, skip_size = 0, ans
-    integer :: l_size = 0, b_size = 256
-
-    integer, dimension(:), allocatable :: lengths
-    integer, dimension(256) :: buffer
-
-    character(len=512) :: input
-
-    open(newunit=io, file='../input/10', status='old', action='read')
-    read(io, '(A)') input
-    close(io)
-
-    l_size = len_trim(input) + 5
-
-    allocate ( lengths(l_size) )
-
-    do idx = 1,l_size - 5
-        lengths(idx) = ichar(input(idx:idx))
-    end do
-
-    lengths(l_size - 4:l_size) = [17,31,73,47,23]
-
-    do idx = 1,64
-        call shuffle(pos, skip_size, lengths, l_size, buffer, b_size)
-    end do
-
-    print *, skip_size
-    print *, pos 
-    print *, 'huh',buffer(1) * buffer(2)
-
-    deallocate (lengths)
-    ans = 1
-end function part_two
-
 program day10
 implicit none
 
-    integer :: p1, p2, part_one, part_two
+    integer           :: p1, part_one
+    character(len=32) :: p2, part_two
 
     p1 = part_one()
     p2 = part_two()
 
     print *, 'Part one: ', p1
+    print *, 'PART TWO WARNING: you have to fill in the empty places with 0 and write letters small'
     print *, 'Part two: ', p2
 end program day10
 
@@ -74,6 +39,54 @@ implicit none
     ans = buffer(1) * buffer(2)
 end function part_one
 
+function part_two() result(hash)
+implicit none
+    
+    integer :: io, idx = 1, pos = 1, skip_size = 0, acc = 0
+    integer :: l_size = 0, b_size = 256
+
+    integer, dimension(:), allocatable :: lengths
+    integer, dimension(256) :: buffer
+
+    character(len=512) :: input
+    character(len=32)  :: hash
+
+    open(newunit=io, file='../input/10', status='old', action='read')
+    read(io, '(A)') input
+    close(io)
+
+    l_size = len_trim(input) + 5
+
+    allocate ( lengths(l_size) )
+
+    do idx = 1,b_size
+        buffer(idx) = idx - 1
+    end do
+
+    do idx = 1,l_size - 5
+        lengths(idx) = ichar(input(idx:idx))
+    end do
+
+    lengths(l_size - 4:l_size) = [17,31,73,47,23]
+
+    do idx = 1,64
+        call shuffle(pos, skip_size, lengths, l_size, buffer, b_size)
+    end do
+
+    do idx = 0,15
+        acc = 0
+        do skip_size =1,16
+            acc = xor(acc, buffer(idx * 16 + skip_size))
+        end do
+
+        write (hash(idx*2+1:idx*2+2), '(Z02)') acc
+    end do
+
+    deallocate (lengths)
+end function part_two
+
+
+
 subroutine shuffle(pos, skip_size, lengths, l_size, buffer, b_size)
 implicit none
     
@@ -90,7 +103,7 @@ integer, intent (out) :: skip_size, pos, buffer(b_size)
         skip_size = skip_size + 1
         i = i + 1
 
-        if (pos > b_size) pos = modulo(pos, b_size + 1) + pos / b_size
+        if (pos > b_size) pos = modulo(pos, b_size + 1) + pos / (b_size + 1)
     end do
 end subroutine shuffle
 
@@ -100,22 +113,20 @@ implicit none
 integer, intent (in)  :: src, dst, size
 integer, intent (inout) :: data(size)
 
-    integer :: lpsz, tmp, sp, dp, ov = 1
+    integer :: lpsz, tmp, sp, dp
 
     lpsz = (dst - src) / 2
     sp = src
     dp = dst
 
-    do while (lpsz > 0)
+    do while (lpsz >= 0)
         if (dp > size) then
-            ov = dp / size
-            dp = modulo(dp, size + 1) + ov
+            dp = modulo(dp, size + 1) + 1
         else if (dp < 1) then
             dp = size
         end if
-        if (sp > size) then
-            sp = modulo(sp, size + 1) + 1
-        end if
+
+        if (sp > size) sp = modulo(sp, size + 1) + 1
 
         tmp = data(sp)
         data(sp) = data(dp)
