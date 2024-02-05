2e060ed827e3967aab76c912a1a308a6af9a14a9
aichingert <aichinger.tobias13@gmail.com>
2024-02-05 14:07:56 +0100

    feat: 2017 [plsql] day 12 setup

--- 2017-rainbow/20-r/20.r
+++ 2017-rainbow/20-r/20.r
@@ -1,1 +1,19 @@
-print("Day 20")
+
+get_input <- function() {
+    chars <- strsplit(readLines("../input/20"), ", ")
+
+    vecs = matrix(numeric(3), nrow = length(chars), ncol = 3)
+
+    for (i in 1:length(chars)) {
+        print(vecs[i])
+        # print(chars[i])
+    }
+
+    return (vecs)
+}
+
+apply <- function(vec) {
+
+}
+
+vec = get_input()
