81e0225e708c6d9c5ee08ebf6ef942b51369b7dc
aichingert <aichinger.tobias13@gmail.com>
2024-02-13 09:15:27 +0100

    feat: 2017 [perl] day 13 part 1

--- 2017-rainbow/13-pearl/13.pl
+++ 2017-rainbow/13-pearl/13.pl
@@ -3,4 +3,37 @@
 use strict;
 use warnings;
 
-print("Hello World\n");
+sub parse
+{
+    open(FH, '<', $_[0]) or die $!;
+
+    my @pos = ();
+    my @dpt = ();
+
+    while(<FH>) {
+        my @vals = split(": ", $_);
+        push(@pos, $vals[0]);
+        push(@dpt, $vals[1]);
+    }
+
+    return \@pos, \@dpt;
+}
+
+sub part_one
+{
+    my @pos = @{$_[0]};
+    my @dpt = @{$_[1]};
+    my $score = 0;
+
+    for (my $i = 0; $i <= $#pos; $i++) {
+        if (!($pos[$i] % (($dpt[$i] - 1) * 2))) {
+            $score += $pos[$i] * $dpt[$i];
+        }
+    }
+
+    return $score;
+}
+
+my (@pos, @dpt) = parse('../input/13');
+
+print("Part one: ", part_one(@pos, @dpt), "\n");
