da7288713d9e7273989fc1b466adc407c5ddf916
aichingert <aichinger.tobias13@gmail.com>
2024-02-20 19:13:03 +0100

    feat: 2017 [haskell] day 15 part 1 horrendously bad

--- 2017-rainbow/15-hs/15.hs
+++ 2017-rainbow/15-hs/15.hs
@@ -1,5 +1,18 @@
 module Main where
 
+import Data.Bits
+
+dropFirst :: [Integer] -> [Integer]
+dropFirst [] = []
+dropFirst (x:xs) = xs
+
+gen :: Int -> Integer -> Integer -> [Integer]
+gen n fac start = let l = take (n + 1) (iterate (\x -> mod (x * fac) 2147483647) start) 
+                    in map (.&. 0xFFFF) (dropFirst l)
+
+partOne :: Int -> Int
+partOne n = length (filter (== 0) (zipWith (xor) (gen n 16807 65) (gen n 48271 8921)))
+
 main :: IO ()
 main = do
-    putStrLn "hello, world"
+    putStrLn ("Part one: " ++ (show (partOne 40000000)))
