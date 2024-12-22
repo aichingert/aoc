31e42140c3098b78148e016d511e692231f86a5f
aichingert <aichinger.tobias13@gmail.com>
2024-12-22 12:10:08 +0100

    feat(comp): added 2024 day 21 part 1

--- 2024/d21-todo/comp/src/main.rs
+++ 2024/d21-todo/comp/src/main.rs
@@ -27,64 +27,15 @@ const NUM: [[char; 5]; 6] = [
     ['#','#','#','#', '#']
 ];
 
-const DIR: [[char; 5]; 6] = [
-    ['#','#','#','#', '#'],
-    ['#','#','#','#', '#'],
+const DIR: [[char; 5]; 4] = [
     ['#','#','#','#', '#'],
     ['#', '#', '^', 'A', '#'],
     ['#', '<', 'v', '>', '#'],
     ['#','#','#','#', '#'],
 ];
 
-const PADS: [[[char; 5]; 6]; 2] = [NUM, DIR];
-
-//fn sol(
-//    s: &mut HashMap<(usize, usize, usize), usize>, 
-//    pad: usize,
-//    code: &str, 
-//    (py, px): V2, 
-//    mut steps: Vec<char>,
-//) -> Vec<Vec<char>> {
-//    if code.is_empty() {
-//        return vec![steps];
-//    }
-//
-//    if &code[..1] == PADS[pad][py][px] {
-//        steps.push('A');
-//        s.insert((code.len() - 1, py, px), steps.len());
-//        return sol(s, pad, &code[1..], (py, px), steps);
-//    }
-//    s.insert((code.len(), py, px), steps.len());
-//
-//
-//    let mut opts = Vec::new();
-//
-//    for (v, dy, dx) in [('v', 1, 0), ('^', #, 0), ('<', 0, #), ('>', 0, 1)] {
-//        let (ny, nx) = ((py as i32 + dy) as usize, (px as i32 + dx) as usize);
-//
-//        if PADS[pad][ny][nx] == '#' {
-//            continue;
-//        }
-//        if let Some(&r) = s.get(&(code.len(), ny, nx)) {
-//            if r <= steps.len() {
-//                continue;
-//            }
-//        }
-//
-//        let mut cp = steps.clone();
-//        cp.push(v);
-//
-//        for ans in sol(s, pad, code, (ny, nx), cp) {
-//            opts.push(ans);
-//        }
-//    }
-//
-//    opts
-//}
-
 fn sol(
     map: &mut HashMap<(usize, usize, usize), usize>, 
-    pad: usize,
     code: &[char], 
     (py, px): V2, 
     mut steps: Vec<char>,
@@ -93,10 +44,10 @@ fn sol(
         return vec![steps];
     }
 
-    if code[0] == PADS[pad][py][px] {
+    if code[0] == NUM[py][px] {
         steps.push('A');
         map.insert((code.len() - 1, py, px), steps.len());
-        return sol(map, pad, &code[1..], (py, px), steps);
+        return sol(map, &code[1..], (py, px), steps);
     }
     map.insert((code.len(), py, px), steps.len());
     let mut opts = Vec::new();
@@ -104,7 +55,7 @@ fn sol(
     for (v, dy, dx) in [('v', 1, 0), ('^', -1, 0), ('<', 0, -1), ('>', 0, 1)] {
         let (ny, nx) = ((py as i32 + dy) as usize, (px as i32 + dx) as usize);
 
-        if PADS[pad][ny][nx] == '#' {
+        if NUM[ny][nx] == '#' {
             continue;
         }
         if let Some(&len) = map.get(&(code.len(), ny, nx)) {
@@ -116,12 +67,29 @@ fn sol(
         let mut cp = steps.clone();
         cp.push(v);
 
-        for ans in sol(map, pad, code, (ny, nx), cp) {
+        for ans in sol(map, code, (ny, nx), cp) {
             opts.push(ans);
         }
     }
 
     opts
+}
+
+fn chain(lookup: &HashMap<(char, char), Vec<char>>, robo: u32, src: char, dst: &[char]) -> usize {
+    if robo == 0 {
+        return lookup.get(&(src, dst[dst.len() - 1])).unwrap().len();
+    }
+
+    let (mut size, mut nxt) = (0, 'A');
+
+    for &cur in dst {
+        let path = lookup.get(&(nxt, cur)).unwrap();
+
+        size += chain(lookup, robo - 1, cur, path);
+        nxt = cur;
+    }
+
+    size
 }
 
 fn main() {
@@ -133,95 +101,84 @@ fn main() {
         let s = ch.iter().filter(|c| c.is_digit(10)).map(|&c| c).collect::<String>();
         let n = s.parse::<usize>().unwrap();
 
-        let mut dom: Vec<Vec<char>> = Vec::new();
-        let mut rfm: Vec<Vec<char>> = Vec::new();
+        let mut dom = Vec::new();
         let mut min = usize::MAX;
 
-        for dor in sol(&mut HashMap::new(), 0, &ch, (4, 3), Vec::new()) {
+        // ['#','#','#','#', '#'],
+        // ['#', '#', '^', 'A', '#'],
+        // ['#', '<', 'v', '>', '#'],
+        // ['#','#','#','#', '#'],
+
+        let lookup = HashMap::from([
+            (('A', '>'), vec!['v', 'A']),
+            (('A', '^'), vec!['<', 'A']),
+            (('A', 'v'), vec!['v', '<', 'A']),
+            (('A', '<'), vec!['v', '<', '<', 'A']),
+            (('A', 'A'), vec!['A']),
+
+            (('^', 'A'), vec!['>', 'A']),
+            (('^', 'v'), vec!['v', 'A']),
+            (('^', '<'), vec!['v', '<', 'A']),
+            (('^', '>'), vec!['v', '>', 'A']),
+            (('^', '^'), vec!['A']),
+
+            (('v', 'A'), vec!['>', '^', 'A']),
+            (('v', '^'), vec!['^', 'A']),
+            (('v', '<'), vec!['<', 'A']),
+            (('v', '>'), vec!['>', 'A']),
+            (('v', 'v'), vec!['A']),
+
+            (('>', 'A'), vec!['^', 'A']),
+            (('>', '^'), vec!['^', '<', 'A']),
+            (('>', '<'), vec!['<', '<', 'A']),
+            (('>', 'v'), vec!['<', 'A']),
+            (('>', '>'), vec!['A']),
+
+            (('<', 'A'), vec!['>', '>', '^', 'A']),
+            (('<', '^'), vec!['>', '^', 'A']),
+            (('<', '>'), vec!['>', '>', 'A']),
+            (('<', 'v'), vec!['>', 'A']),
+            (('<', '<'), vec!['A']),
+        ]);
+
+        for dor in sol(&mut HashMap::new(), &ch, (4, 3), Vec::new()) {
             if min > dor.len() {
-                min = min.min(dor.len());
+                min = dor.len();
                 dom = vec![dor];
             } else if min == dor.len() {
                 dom.push(dor);
             }
         }
-            
 
         min = usize::MAX;
-        println!("unlock dor:");
 
-        for d in dom {
-            println!("{d:?}");
+        for mut door in dom {
+            //let size = chain(&lookup, 25, 'A', &door);
 
-            for rf in sol(&mut HashMap::new(), 1, &d, (3, 3), Vec::new()) {
-                for rs in sol(&mut HashMap::new(), 1, &rf, (3, 3), Vec::new()) {
-                    if min > n * rs.len() {
-                        min = n * rs.len();
-                        println!("{n} - {}", rs.len());
-                    }
+            //println!("{code} - {:?}", size);
+            //min = min.min(size);
+
+            for i in 0..2 {
+                let mut next = Vec::new();
+                let mut start = 'A';
+
+                for &c in &door {
+                    let path = lookup.get(&(start, c)).unwrap();
+                    start = c;
+
+                    next.extend_from_slice(path);
                 }
+
+                door = next;
+                println!("{}", door.len());
             }
+
+            println!("{code} - {:?}", door.len());
+            min = min.min(door.len());
         }
 
-        a += min;
-
-        /*
-        min = usize::MAX;
-        println!("solve robo:");
-        
-        for r in rfm {
-            println!("{r:?}");
-            for rs in sol(&mut HashMap::new(), 1, &r, (4, 3), Vec::new()) {
-                f = f.min(rs.len());
-                min = min.min(n * rs.len());
-            }
-        }
-        println!("{} : {}", code, f);
-
-
-        min = usize::MAX;
-        let mut r1 = String::new();
-
-        for cur in sol(&mut HashMap::new(), 1, &dor, (4, 3), Vec::new()) {
-            if cur.len() < min {
-                min = cur.len();
-                r1 = cur.into_iter().collect::<String>();
-            }
-        }
-
-        min = usize::MAX;
-        let mut r2 = String::new();
-
-        for cur in sol(&mut HashMap::new(), 1, &r1, (4, 3), Vec::new()) {
-            if cur.len() < min {
-                min = cur.len();
-                r2 = cur.into_iter().collect::<String>();
-            }
-        }
-
-        let s = code.chars().filter(|c| c.is_digit(10)).collect::<String>();
-        let n = s.parse::<usize>().unwrap();
-
-        println!('{}', r2.len());
-        println!('{}', n);
-        println!('{}', n * r2.len());
-        a += n * r2.len();
-        */
+        a += min * n;
     }
-
-    // v<<A>>^A<A>AvA<^AA>A<vAAA>^A
-    // <A^A>^^AvvvA
-    // <A^A^^>AvvvA
-
-    // ^<AAA>Av<<A>^>Av<AAA^>AvA^A
-    // <v<A>>^AAAvA^A<vA<AA>>^AvAA<^A>A<v<A>A>^AAAvA<^A>A<vA>^A<A>A
-
-    // v<<A>>^A<A>AvA<^AA>A<vAAA>^A
-    // v<<A>^>A<A>A<AAv>A^Av<AAA^>A
-
-
-    // <vA<AA>>^AvAA<^A>A<v<A>>^AvA^A<vA>^A<v<A>^A>AAvA^A<v<A>A>^AAAvA<^A>A
-    // v<A<AA>^>AvA^<Av>A^Av<<A>^>AvA^Av<<A>^>AAv<A>A^A<A>Av<A<A>^>AAA<Av>A^A
 
     println!("{inp:?}");
     println!("{a:?}");
