e4dd8fefcd1359ca0b620ed31f40e52d657d3265
aichingert <aichinger.tobias13@gmail.com>
2024-12-23 19:15:08 +0100

    feat(comp): 2024 day 23

--- 2024/d23-lan_party/comp/src/main.rs
+++ 2024/d23-lan_party/comp/src/main.rs
@@ -1,27 +1,33 @@
 use std::collections::{HashMap, HashSet};
 
-// A <-> B
-// C <-> A
-// B <-> C
+fn sol<'a>(
+    depth: u32,
+    visited: &mut HashSet<(&'a str, u32)>,
+    connection: &'a str, 
+    connections: HashSet<&'a str>,
+    graph: &'a HashMap<&'a str, Vec<&'a str>>
+) -> Vec<HashSet<&'a str>> {
+    if !visited.insert((connection, depth)) {
+        return vec![];
+    }
 
-//for i in &c {
-//    for fjump in l.get(i).unwrap() {
-//        for sjump in l.get(fjump).unwrap() {
-//            if sjump == i {
-//                continue;
-//            }
-//
-//            if l.get(sjump).unwrap().contains(i) {
-//                let mut int = [i, fjump, sjump];
-//                int.sort();
-//                pairs.insert(int);
-//            }
-//        }
-//    }
-//}
+    let mut lans = Vec::new();
 
-fn sol<'a>(computers: &'a HashSet<&'a str>) -> u32 {
-    0
+    'out: for jump in &graph[connection] {
+        for connected in &connections {
+            if !graph[connected].contains(jump) {
+                continue 'out;
+            }
+        }
+        
+        let mut cp = connections.clone();
+        cp.insert(jump);
+
+        lans.push(cp.clone());
+        lans.extend_from_slice(&sol(depth + 1, visited, jump, cp, graph));
+    }
+
+    lans
 }
 
 
@@ -29,46 +35,48 @@ fn main() {
     let inp = std::fs::read_to_string("../../../input/2024/23").unwrap();
 
     let mut c = HashSet::new();
-    let mut l: HashMap<&str, Vec<&str>> = HashMap::new();
+    let mut g: HashMap<&str, Vec<&str>> = HashMap::new();
 
     for line in inp.lines().filter(|l| !l.is_empty()) {
         let (lhs, rhs) = line.split_once("-").unwrap();
         c.insert(lhs);
         c.insert(rhs);
 
-        l.entry(lhs).and_modify(|s| s.push(rhs)).or_insert(vec![rhs]);
-        l.entry(rhs).and_modify(|s| s.push(lhs)).or_insert(vec![lhs]);
+        g.entry(lhs).and_modify(|s| s.push(rhs)).or_insert(vec![rhs]);
+        g.entry(rhs).and_modify(|s| s.push(lhs)).or_insert(vec![lhs]);
     }
 
-    println!("{}", sol(&c));
 
     let mut pairs = HashSet::new();
 
     for i in &c {
-        for fjump in l.get(i).unwrap() {
-            for sjump in l.get(fjump).unwrap() {
-                if sjump == i {
-                    continue;
-                }
+        let graphs = sol(0, &mut HashSet::new(), i, HashSet::from([*i]), &g);
 
-                if l.get(sjump).unwrap().contains(i) {
-                    let mut int = [i, fjump, sjump];
-                    int.sort();
-                    pairs.insert(int);
-                }
-            }
+        for graph in graphs {
+            let mut vec = graph.into_iter().collect::<Vec<_>>();
+            vec.sort();
+            pairs.insert(vec);
         }
     }
 
+    let mut ma = 0;
     let mut p1 = 0;
+    let mut p2 = Vec::new();
 
-    'out: for pair in &pairs {
-        for m in pair {
+    for pair in pairs {
+        for m in &pair {
             if m.starts_with("t") {
                 p1 += 1;
-                continue 'out;
+                break;
             }
         }
+
+        if ma < pair.len() {
+            ma = pair.len();
+            p2 = pair;
+        }
     }
+
     println!("p1: {}", p1);
+    println!("p2: {}", p2.join(","));
 }
